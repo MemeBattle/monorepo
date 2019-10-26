c0d7f056e9f78808bafc82aee4ef20218ba5a93c
Artem Zverev <themezv@gmail.com>
2019-10-26 15:19:53 +0700

    front work

--- apps/ligretto-backend/src/socketIoServer.ts
+++ apps/ligretto-backend/src/socketIoServer.ts
@@ -3,7 +3,12 @@ import * as SocketIo from 'socket.io'
 const server = SocketIo(3005)
 
 server.on('connection', socket => {
-  socket.on('event', data => {
-    console.log('event', data)
+  socket.on('message', data => {
+    console.log('message', data)
+  })
+
+  socket.on('echo', data => {
+    console.log('ECHO', data)
+    socket.emit(data.type, data.payload)
   })
 })
