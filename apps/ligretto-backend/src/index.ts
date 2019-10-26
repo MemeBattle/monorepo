3af721352b2a502a700ed8632a0094d270ed2bf5
Artem Zverev <themezv@gmail.com>
2019-10-26 19:34:29 +0700

    Load env

---
+++ apps/ligretto-backend/src/index.ts
@@ -0,0 +1,7 @@
+import * as dotenv from 'dotenv'
+dotenv.config()
+
+if (!process.env) {
+  throw Error('Env is not valid. Run yarn env')
+}
+import './socketIoServer'
--- apps/ligretto-backend/src/socketIoServer.ts
+++ apps/ligretto-backend/src/socketIoServer.ts
@@ -1,6 +1,7 @@
 import * as SocketIo from 'socket.io'
+import { SOCKET_PORT } from './config'
 
-const server = SocketIo(3005)
+const server = SocketIo(SOCKET_PORT)
 
 server.on('connection', socket => {
   socket.on('message', data => {
