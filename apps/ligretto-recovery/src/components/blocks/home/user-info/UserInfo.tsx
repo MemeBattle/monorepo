54217894c49281193cd3abf5d9dc985ecc0329ef
bondiano <batbondik0@gmail.com>
2019-11-04 17:54:46 +0700

    fear(ligretto/homepage): add all basic components

--- apps/ligretto-recovery/src/components/blocks/home/user-info/UserInfo.tsx
+++ apps/ligretto-recovery/src/components/blocks/home/user-info/UserInfo.tsx
@@ -1,14 +1,11 @@
 import React from 'react'
 
-import userAvatar from 'assets/icons/avatars/1.svg' // TODO: get avatars from avatar collection
-
-import { Avatar } from 'components/blocks/shared/avatar'
-
 import { UsernameField } from './username-field'
+import { AvatarSelect } from './avatar-select'
 
 const UserInfo = () => (
   <section>
-    <Avatar src={userAvatar} />
+    <AvatarSelect />
     <UsernameField />
   </section>
 )
