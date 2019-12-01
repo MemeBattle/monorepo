1f4e77d8d8521a4162192ec84b89d2058b63b9eb
bondiano <batbondik0@gmail.com>
2019-12-01 14:52:43 +0700

    Fix redux createAction typing

--- apps/ligretto-recovery/src/types/actions.ts
+++ apps/ligretto-recovery/src/types/actions.ts
@@ -1,13 +1,21 @@
-import { Action as ReduxAction } from 'redux'
+interface ReduxAction<T extends string> {
+  type: T
+}
 
-export interface Action<Type, Payload = undefined> extends ReduxAction<Type> {
+export interface Action<Type extends string, Payload = void> extends ReduxAction<Type> {
   payload: Payload
 }
+
+export type AnyAction = Action<any, any>
+
+type StingTypeField = {
+  type: string
+}
+
+export type LookupStringTypeField<T, K> = K extends keyof T ? (T extends StingTypeField ? T[K] : never) : never
 
 export interface ErrorPayload {
   error: any
 }
 
-export type ErrorAction<Type> = Action<Type, ErrorPayload>
-
-export type AnyAction = Action<any, any>
+export type ErrorAction<Type extends string> = Action<Type, ErrorPayload>
