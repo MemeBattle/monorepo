c8c879757b0281d689ce5c1aab418b23245c43ea
Artem Zverev <themezv@gmail.com>
2020-02-24 00:36:42 +0700

    opponents cards positions

--- apps/ligretto-recovery/src/components/blocks/opponent-cards/OpponentCards.tsx
+++ apps/ligretto-recovery/src/components/blocks/opponent-cards/OpponentCards.tsx
@@ -1,19 +1,40 @@
 import React from 'react'
+import cn from 'classnames'
 import { CardContainer } from 'containers/card'
 import { CardPositions } from 'types/entities/card-model'
 import styles from './OpponentCards.module.scss'
 
 const WrappedCard: React.FC<{ cardPosition: CardPositions }> = ({ cardPosition }) => (
-  <div className={styles.card}>
-    <CardContainer cardPosition={cardPosition} />
-  </div>
+  <CardContainer className={styles.card} cardPosition={cardPosition} />
 )
 
-export const OpponentCards: React.FC = () => (
-  <div className={styles.opponentCards}>
+export enum PositionOnTable {
+  Left = 'left',
+  Right = 'right',
+  Top = 'top',
+  LeftTopCorner = 'leftTopCorner',
+  RightTopCorner = 'rightTopCorner',
+}
+
+const stylesByPosition = {
+  [PositionOnTable.Left]: styles.positionLeft,
+  [PositionOnTable.Right]: styles.positionRight,
+  [PositionOnTable.LeftTopCorner]: styles.positionLeftTopCorner,
+  [PositionOnTable.RightTopCorner]: styles.positionRightTopCorner,
+  [PositionOnTable.Top]: styles.positionTop,
+}
+
+export interface OpponentCardsProps {
+  positionOnTable: PositionOnTable
+  className?: string
+}
+export const OpponentCards: React.FC<OpponentCardsProps> = ({ positionOnTable, className }) => (
+  <div className={cn(styles.opponentCards, stylesByPosition[positionOnTable], className)}>
     <WrappedCard cardPosition={CardPositions.a} />
     <WrappedCard cardPosition={CardPositions.b} />
     <WrappedCard cardPosition={CardPositions.c} />
     <WrappedCard cardPosition={CardPositions.d} />
   </div>
 )
+
+OpponentCards.displayName = 'OpponentCards'
