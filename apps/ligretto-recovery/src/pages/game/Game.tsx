54155c8cd5167e6514b50c90b2d4bb175dea3d14
Artem Zverev <themezv@gmail.com>
2020-02-29 16:22:10 +0700

    Opponents waiting in game lobby

--- apps/ligretto-recovery/src/pages/game/Game.tsx
+++ apps/ligretto-recovery/src/pages/game/Game.tsx
@@ -4,15 +4,18 @@ import { GameCoverScreen } from 'components/screens/game-cover-screen/GameCoverS
 import { CardsPanel } from 'components/blocks/cards-panel'
 import { OpponentCards } from 'components/blocks/opponent-cards'
 import { OpponentWaiting } from 'components/blocks/opponent-waiting'
-import { RoomGrid, PositionOnTable, RenderChildren, RenderMultiplyChildren, isMultiplyRenderChildren } from 'components/base/room-grid'
+import { isMultiplyRenderChildren, PositionOnTable, RenderChildren, RenderMultiplyChildren, RoomGrid } from 'components/base/room-grid'
 import { TableCards } from 'components/blocks/table-cards'
 import { PlayerReadyButton } from 'components/blocks/player-ready-button'
 import styles from './Game.module.scss'
+import { OpponentStatus } from '../../components/blocks/opponent-waiting/OpponentWaiting'
 
 const opponents = [0, 1, 2]
 
 const renderOpponent: RenderChildren = (positionOnTable: PositionOnTable) => <OpponentCards positionOnTable={positionOnTable} />
-const renderOpponentWaiting: RenderChildren = (positionOnTable: PositionOnTable) => <OpponentWaiting positionOnTable={positionOnTable} />
+const renderOpponentWaiting: RenderChildren = (positionOnTable: PositionOnTable) => (
+  <OpponentWaiting opponentStatus={OpponentStatus.Waiting} positionOnTable={positionOnTable} />
+)
 
 export const GamePage: React.FC = () => {
   const [isGameStarted, setGameStarted] = React.useState(false)
