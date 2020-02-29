844d037f6c2d2ffb8917c668b2f0fa796c93ed3f
Artem Zverev <themezv@gmail.com>
2020-02-29 16:03:09 +0700

    Player ready to play in lobby

--- apps/ligretto-recovery/src/components/blocks/opponent-waiting/OpponentWaiting.tsx
+++ apps/ligretto-recovery/src/components/blocks/opponent-waiting/OpponentWaiting.tsx
@@ -1,9 +1,24 @@
 import React from 'react'
+import cn from 'classnames'
 import opponentAvatar from 'assets/icons/avatars/2.svg' // TODO: get avatar from avatars collection
-import { Avatar } from '../shared/avatar'
+import { Avatar, AvatarSize } from '../shared/avatar'
+import { PositionOnTable } from 'components/base/room-grid'
+import styles from './OpponentWaiting.module.scss'
 
-export const OpponentWaiting = () => (
-  <div>
-    <Avatar src={opponentAvatar} />
+export interface OpponentWaitingProps {
+  positionOnTable: PositionOnTable
+}
+
+const classNameByPositionOnTable = {
+  [PositionOnTable.Left]: styles.left,
+  [PositionOnTable.Right]: styles.right,
+  [PositionOnTable.Top]: styles.top,
+  [PositionOnTable.RightTopCorner]: styles.rightTopCorner,
+  [PositionOnTable.LeftTopCorner]: styles.leftTopCorner,
+}
+
+export const OpponentWaiting: React.FC<OpponentWaitingProps> = ({ positionOnTable }) => (
+  <div className={cn(styles.opponentWaiting, classNameByPositionOnTable[positionOnTable])}>
+    <Avatar src={opponentAvatar} size={AvatarSize.Small} />
   </div>
 )
---
+++ apps/ligretto-recovery/src/components/blocks/player-ready-button/index.ts
@@ -0,0 +1,1 @@
+export { PlayerReadyButton } from './PlayerReadyButton'
