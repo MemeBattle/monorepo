8a348b1a43f06181b0608461da5341f3ba4d6ba2
Artem Zverev <themezv@gmail.com>
2020-04-22 02:19:24 +0700

    Improve UI of main page

--- apps/ligretto-recovery/src/pages/Home.tsx
+++ apps/ligretto-recovery/src/pages/Home.tsx
@@ -1,15 +1,15 @@
 import React from 'react'
 
-import { MainCoverScreen } from 'components/screens/main-cover-screen/MainCoverScreen'
+import { GameCoverScreen } from 'components/screens/game-cover-screen/GameCoverScreen'
 import { Menu } from 'components/blocks/home/menu'
 import { UserInfo } from 'components/blocks/home/user-info'
 import { HomePageWrapper } from 'components/blocks/home/homepage-wrapper'
 
 export const HomePage: React.FC = () => (
-  <MainCoverScreen>
+  <GameCoverScreen>
     <HomePageWrapper>
       <UserInfo />
       <Menu />
     </HomePageWrapper>
-  </MainCoverScreen>
+  </GameCoverScreen>
 )
