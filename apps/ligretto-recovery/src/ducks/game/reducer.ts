c99a1d7dbace862f4c3023d5bbe2697238e30219
Artem Zverev <themezv@gmail.com>
2020-05-14 02:55:50 +0700

    Add mappers for player and opponents cards. Remove frontend local models duplications

--- apps/ligretto-recovery/src/ducks/game/reducer.ts
+++ apps/ligretto-recovery/src/ducks/game/reducer.ts
@@ -1,11 +1,12 @@
-import { Game, GameStatus } from '@memebattle/ligretto-shared'
-import { UpdateGameAction, GameTypes } from './types'
+import { CardColors, Game, GameStatus } from '@memebattle/ligretto-shared'
+import { GameTypes, UpdateGameAction } from './types'
 
 export type GameState = {
   name: Game['name']
   id: Game['id']
   config: Game['config']
   status: Game['status']
+  playerColor: CardColors
 }
 
 const initialState: GameState = {
@@ -16,6 +17,7 @@ const initialState: GameState = {
     cardsCount: 0,
     playersMaxCount: 4,
   },
+  playerColor: CardColors.empty,
 }
 
 export const gameReducer = (state: GameState = initialState, action: UpdateGameAction) => {
