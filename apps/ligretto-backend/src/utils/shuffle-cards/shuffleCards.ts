3118be392df755e3bdd3e6bd6adc5ad6b00ca21c
Artem Zverev <themezv@gmail.com>
2020-05-23 17:14:43 +0700

    Start game cards on backend

---
+++ apps/ligretto-backend/src/utils/shuffle-cards/shuffleCards.ts
@@ -0,0 +1,7 @@
+import { Card } from '@memebattle/ligretto-shared'
+import { shuffle } from 'lodash'
+
+/**
+ * Return a new array with shuffled cards
+ */
+export const shuffleCards = (cards: Card[]): Card[] => shuffle(cards)
