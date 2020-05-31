0e6a05c8b6a90259b06f1a26fbb2fca9fcc6b6e4
Artem Zverev <themezv@gmail.com>
2020-05-31 20:08:56 +0700

    Map table cards

--- apps/ligretto-recovery/src/components/blocks/game/table-cards/TableCards.tsx
+++ apps/ligretto-recovery/src/components/blocks/game/table-cards/TableCards.tsx
@@ -1,15 +1,12 @@
 import React from 'react'
 import { CardContainer } from 'containers/card'
-import { CardPositions } from '@memebattle/ligretto-shared'
-import { MAX_CARDS_ON_TABLE, TABLE_CARDS_PREFIX } from 'config'
 import styles from './TableCards.module.scss'
-
-const tableCardsPositions = new Array(MAX_CARDS_ON_TABLE).fill(1).map((_, index) => `${TABLE_CARDS_PREFIX}${index}`)
+import { tablePositions } from 'utils/constants/tableCardsPositions'
 
 export const TableCards: React.FC = () => (
   <div className={styles.tableCards}>
-    {tableCardsPositions.map(cardPosition => (
-      <CardContainer className={styles.card} cardPosition={cardPosition as CardPositions} key={cardPosition} />
+    {tablePositions.map(cardPosition => (
+      <CardContainer className={styles.card} cardPosition={cardPosition} key={cardPosition} />
     ))}
   </div>
 )
