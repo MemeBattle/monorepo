d3eb58a5aa18de9957ed39c1fcc4ced60c0b9437
Artem Zverev <themezv@gmail.com>
2020-06-03 00:29:00 +0700

    Put card

--- apps/ligretto-recovery/src/ducks/cards/actions.ts
+++ apps/ligretto-recovery/src/ducks/cards/actions.ts
@@ -1,6 +1,5 @@
 import { createAction } from '@memebattle/redux-utils'
-import { CardsTypes, PushCardAction, TapCardAction, TapCardEmitAction } from './types'
+import { CardsTypes, PushCardAction, TapCardAction } from './types'
 
 export const pushCardsAction = createAction<PushCardAction>(CardsTypes.PUSH_CARDS)
 export const tapCardAction = createAction<TapCardAction>(CardsTypes.TAP_CARD)
-export const tapCardEmitAction = createAction<TapCardEmitAction>(CardsTypes.TAP_CARD_EMIT)
