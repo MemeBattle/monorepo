ae098ce7d24e8488479b9f806e12890ce589c140
Artem Zverev <themezv@gmail.com>
2020-06-27 03:20:14 +0700

    Add rollup.config.js to git ignore for fix CI

---
+++ packages/ligretto-ui/rollup.config.js
@@ -0,0 +1,23 @@
+import resolve from '@rollup/plugin-node-resolve'
+import peerDepsExternal from 'rollup-plugin-peer-deps-external'
+import typescript from 'rollup-plugin-typescript2'
+import commonjs from '@rollup/plugin-commonjs'
+
+const packageJson = require('./package.json')
+
+export default {
+  input: 'src/index.ts',
+  output: [
+    {
+      file: packageJson.main,
+      format: 'cjs',
+      sourcemap: true,
+    },
+    {
+      file: packageJson.module,
+      format: 'esm',
+      sourcemap: true,
+    },
+  ],
+  plugins: [peerDepsExternal(), resolve(), commonjs(), typescript({ tsconfig: 'tsconfig.json', useTsconfigDeclarationDir: true })],
+}
