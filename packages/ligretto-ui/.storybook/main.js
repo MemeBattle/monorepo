3bb77dd49cf46bbb2078498c5e3e7c6a5e06cfcd
Artem Zverev <themezv@gmail.com>
2020-06-27 23:18:30 +0700

    Add configs for storybook

---
+++ packages/ligretto-ui/.storybook/main.js
@@ -0,0 +1,32 @@
+const path = require('path')
+
+module.exports = {
+  addons: ['@storybook/addon-docs'],
+  stories: ['../src/**/*.stories.[tj]sx'],
+  webpackFinal: async config => {
+    config.module.rules.push({
+      test: /\.(ts|tsx)$/,
+      loader: require.resolve('babel-loader'),
+      options: {
+        presets: [['react-app', { flow: false, typescript: true }]],
+      },
+    })
+    config.module.rules.push({
+      test: /\.(ts|tsx)$/,
+      use: [
+        {
+          loader: require.resolve('ts-loader'),
+        },
+        {
+          loader: require.resolve('react-docgen-typescript-loader'),
+          options: {
+            tsconfigPath: path.resolve(__dirname, '../tsconfig.json'),
+            shouldExtractLiteralValuesFromEnum: true,
+          },
+        },
+      ],
+    })
+    config.resolve.extensions.push('.ts', '.tsx')
+    return config
+  },
+}
