7b04b672f866366f69230ad2b9079c482245b802
Artem Zverev <themezv@gmail.com>
2020-06-27 17:45:58 +0700

    One game update

--- apps/ligretto-backend/src/controllers/gameplay-controller.ts
+++ apps/ligretto-backend/src/controllers/gameplay-controller.ts
@@ -12,12 +12,12 @@ import {
 } from '@memebattle/ligretto-shared'
 import { TYPES } from '../types'
 import { Gameplay } from '../gameplay/gameplay'
-import { GameplayOutput } from '../gameplay/gameplay-output'
+import { GameService } from '../entities/game/game.service'
 
 @injectable()
 export class GameplayController extends Controller {
   @inject(TYPES.Gameplay) private gameplay: Gameplay
-  @inject(TYPES.GameplayOutput) private gameplayOutput: GameplayOutput
+  @inject(TYPES.GameService) private gameService: GameService
 
   handlers = {
     [GameplayTypes.START_GAME]: (socket, action) => this.startGame(socket, action),
@@ -28,23 +28,24 @@ export class GameplayController extends Controller {
     [GameplayTypes.TAKE_FROM_STACK_DECK]: (socket: Socket, action) => this.takeCardFromStackDeck(socket, action),
   }
 
+  private async updateGame(socket: Socket, gameId: string) {
+    const game = await this.gameService.getGame(gameId)
+    const action = updateGameAction(game)
+    socket.to(gameId).emit('event', action)
+    socket.emit('event', action)
+  }
+
   private async startGame(socket: Socket, action: StartGameEmitAction) {
     const gameId = action.payload.gameId
 
-    this.gameplayOutput.listenGame(gameId, game => {
-      console.log('updateGame', game)
-      const action = updateGameAction(game)
-      socket.to(gameId).emit('event', action)
-      socket.emit('event', action)
-    })
-
     await this.gameplay.startGame(gameId)
+
+    await this.updateGame(socket, gameId)
   }
 
   private async endGame(socket: Socket, action) {
     const gameId = action.payload.gameId
 
-    this.gameplayOutput.unlistenGame(action.payload.gameId)
     const results = await this.gameplay.endGame(gameId)
 
     socket.to(gameId).emit('event', results)
@@ -54,12 +55,14 @@ export class GameplayController extends Controller {
     const { gameId, cardIndex } = action.payload
 
     await this.gameplay.playerPutCard(gameId, socket.id, cardIndex)
+    await this.updateGame(socket, gameId)
   }
 
   private async takeCardFromLigrettoDeck(socket: Socket, action: TakeFromLigrettoDeckAction) {
     const { gameId } = action.payload
 
     await this.gameplay.playerTakeFromLigrettoDeck(gameId, socket.id)
+    await this.updateGame(socket, gameId)
   }
 
   private async takeCardFromStackDeck(socket: Socket, action: TakeFromStackDeckAction) {
@@ -67,11 +70,13 @@ export class GameplayController extends Controller {
 
     console.log('takeCardFromStackDeck', action)
     await this.gameplay.playerTakeFromStackDeck(gameId, socket.id)
+    await this.updateGame(socket, gameId)
   }
 
   private async putCardFromStackOpenDeck(socket: Socket, action: PutCardFromStackOpenDeck) {
     const { gameId } = action.payload
     console.log('putCardFromStackOpenDeck', action)
     await this.gameplay.playerPutFromStackOpenDeck(gameId, socket.id)
+    await this.updateGame(socket, gameId)
   }
 }
