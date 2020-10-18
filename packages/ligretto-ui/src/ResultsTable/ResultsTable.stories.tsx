4b648fe4684e4b36a369a51fceb2c95e322588eb
Artem Zverev <themezv@gmail.com>
2020-10-19 01:02:47 +0700

    Add results table in lobby between rounds

---
+++ packages/ligretto-ui/src/ResultsTable/ResultsTable.stories.tsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import { ResultsTable } from './ResultsTable'
+
+export default {
+  title: 'ResultsTable',
+}
+
+const players = [
+  { position: 1, username: 'ThemeZV', roundPoints: 12, totalPoints: 22 },
+  { position: 2, username: 'ThemeZV2', roundPoints: -4, totalPoints: 20 },
+  { position: 3, username: 'ThemeZV3', roundPoints: 0, totalPoints: 8 },
+  { position: 4, username: 'ThemeZV4', roundPoints: 1, totalPoints: 2 },
+]
+
+export const DefaultView = () => <ResultsTable players={players} />
