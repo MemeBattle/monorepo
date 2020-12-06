a1e2ed504f87693373765ed2dfa508ffe002b794
Artem Zverev <artem.zverev@abbyy.com>
2020-12-06 23:54:21 +0700

    Responsive rooms list

--- apps/ligretto-recovery/src/ducks/rooms/selectors.ts
+++ apps/ligretto-recovery/src/ducks/rooms/selectors.ts
@@ -3,3 +3,4 @@ import { All } from 'types/store'
 export const selectRoomsList = (state: All) => state.rooms.ids.map(id => state.rooms.byId[id])
 export const selectIsLoading = (state: All) => state.rooms.isLoading
 export const selectSearch = (state: All) => state.rooms.search
+export const selectIsRoomsListEmpty = (state: All) => state.rooms.ids.length === 0
