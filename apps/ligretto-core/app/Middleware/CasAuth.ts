acddcca85ed25f19d5e13cdf6c2d81003db86864
Artem Zverev <artem.zverev@abbyy.com>
2021-01-10 02:14:32 +0700

    Add login and signup base

---
+++ apps/ligretto-core/app/Middleware/CasAuth.ts
@@ -0,0 +1,8 @@
+import { HttpContextContract } from '@ioc:Adonis/Core/HttpContext'
+
+export default class CasAuth {
+  public async handle ({}: HttpContextContract, next: () => Promise<void>) {
+    console.log('CasAuth')
+    await next()
+  }
+}
