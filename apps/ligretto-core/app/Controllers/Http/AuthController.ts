27d5b4bb60e9a500473a95d09231a817b72c80eb
Artem Zverev <artem.zverev@abbyy.com>
2021-01-10 02:14:32 +0700

    Add login and signup base

---
+++ apps/ligretto-core/app/Controllers/Http/AuthController.ts
@@ -0,0 +1,26 @@
+import { HttpContextContract } from '@ioc:Adonis/Core/HttpContext'
+import {login, signUp} from '@ioc:CasServices'
+import LoginValidator from '../../Validators/LoginValidator'
+import SignUpValidator from "../../Validators/SignUpValidator";
+
+export default class AuthController {
+  async login({ request }: HttpContextContract) {
+    const data = await request.validate(LoginValidator)
+    const loginResult = await login(data)
+    if (loginResult.success) {
+      return loginResult.data
+    } else {
+      throw Error
+    }
+  }
+
+  async signUp({request}: HttpContextContract) {
+    const data = await request.validate(SignUpValidator)
+    const signUpResult = await signUp({username: data.email, email: data.email, password: data.password})
+    if (signUpResult.success) {
+      return signUpResult.data
+    } else {
+      throw Error
+    }
+  }
+}
---
+++ apps/ligretto-core/providers/CasServiceProvider.ts
@@ -0,0 +1,79 @@
+import {createCasServices, LoginCredentials, SignUpCredentials} from '@memebattle/cas-services'
+import {ApplicationContract} from '@ioc:Adonis/Core/Application'
+import Logger from '@ioc:Adonis/Core/Logger'
+import {Services} from '@ioc:CasServices'
+
+/*
+|--------------------------------------------------------------------------
+| Provider
+|--------------------------------------------------------------------------
+|
+| Your application is not ready when this file is loaded by the framework.
+| Hence, the level imports relying on the IoC container will not work.
+| You must import them inside the life-cycle methods defined inside
+| the provider class.
+|
+| @example:
+|
+| public async ready () {
+|   const Database = (await import('@ioc:Adonis/Lucid/Database')).default
+|   const Event = (await import('@ioc:Adonis/Core/Event')).default
+|   Event.on('db:query', Database.prettyPrint)
+| }
+|
+*/
+export default class CasServiceProvider {
+  constructor (protected application: ApplicationContract) {
+    this.application = application
+  }
+
+  public static needsApplication = true
+
+  public async register () {
+    const Env = (await import('@ioc:Adonis/Core/Env')).default
+    const partnerId = Env.get('PARTNER_ID')
+    const publicKey = Env.get('CAS_PUBLIC_KEY')
+    const casURI = Env.get('CAS_URI')
+
+    this.application.container.bind('CasServices', (): Services => {
+      const services = createCasServices({partnerId, casURI, publicKey})
+      const login = async (credentials: LoginCredentials) => {
+        try {
+          const result = await services.loginService(credentials)
+
+          return result.data
+        } catch (e) {
+          Logger.error(e)
+          throw Error(e)
+        }
+      }
+
+      const signUp = async (credentials: SignUpCredentials) => {
+        try {
+          const result = await services.signUpService(credentials)
+
+          return result.data
+        } catch (e) {
+          Logger.error(e)
+          throw Error(e)
+        }
+      }
+
+      const verifyToken = (token: string) => services.verifyToken(token)
+
+      return { login, signUp, verifyToken }
+    })
+  }
+
+  public async boot () {
+    // All bindings are ready, feel free to use them
+  }
+
+  public async ready () {
+    // App is ready
+  }
+
+  public async shutdown () {
+    // Cleanup, since app is going down
+  }
+}
