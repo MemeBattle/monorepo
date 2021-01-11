1a7aa8a480a9272556bbe7bf2ca5d710be65f92a
sdiubin <stepan.diubin@abbyy.com>
2021-01-11 20:27:45 +0700

    Add prerender. Add final-form

--- apps/auth-front/src/pages/RegisterPage.tsx
+++ apps/auth-front/src/pages/RegisterPage.tsx
@@ -1,51 +1,92 @@
 import { memo } from 'react'
 import { Link } from 'react-router-dom'
+import { Form, Field } from 'react-final-form'
 import { Paper } from '../components/Paper'
 import { Button, Container, Input, PasswordInput } from '@memebattle/ligretto-ui'
 import { t } from '../utils/i18n'
 import { ROUTES } from '../constants/routes'
 
-// eslint-disable-next-line react/display-name
 export const RegisterPage = memo(() => (
   <Container component="main" maxWidth="xs">
-    <Paper>
-      <Input
-        variant="outlined"
-        margin="normal"
-        required
-        fullWidth
-        id="username"
-        label={t.register.username}
-        name="username"
-        autoComplete="username"
-        autoFocus
-      />
-      <Input variant="outlined" margin="normal" required fullWidth id="email" label={t.register.email} name="email" autoComplete="email" />
-      <PasswordInput
-        variant="outlined"
-        margin="normal"
-        required
-        fullWidth
-        name="password"
-        label={t.login.password}
-        id="password"
-        autoComplete="current-password"
-      />
-      <PasswordInput
-        variant="outlined"
-        margin="normal"
-        required
-        fullWidth
-        name="confirm password"
-        label={t.register.confirmPassword}
-        id="password2"
-      />
-      <br />
-      <Button type="submit" fullWidth variant="contained" color="primary" size="large">
-        {t.register.submit}
-      </Button>
-      <br />
-      <Link to={ROUTES.LOGIN}>{t.register.linkToLogin}</Link>
-    </Paper>
+    <Form
+      onSubmit={values => console.log(values)}
+      render={({ handleSubmit }) => (
+        <form onSubmit={handleSubmit}>
+          <Paper>
+            <Field
+              name="username"
+              render={({ input }) => (
+                <Input
+                  {...input}
+                  variant="outlined"
+                  margin="normal"
+                  required
+                  fullWidth
+                  id="username"
+                  label={t.register.username}
+                  name="username"
+                  autoComplete="username"
+                  autoFocus
+                />
+              )}
+            />
+            <Field
+              name="email"
+              render={({ input }) => (
+                <Input
+                  {...input}
+                  variant="outlined"
+                  margin="normal"
+                  required
+                  fullWidth
+                  id="email"
+                  label={t.register.email}
+                  name="email"
+                  autoComplete="email"
+                />
+              )}
+            />
+            <Field
+              name="password"
+              render={({ input }) => (
+                <PasswordInput
+                  {...input}
+                  variant="outlined"
+                  margin="normal"
+                  required
+                  fullWidth
+                  name="password"
+                  label={t.login.password}
+                  id="password"
+                  autoComplete="current-password"
+                />
+              )}
+            />
+            <Field
+              name="confirmPassword"
+              render={({ input }) => (
+                <PasswordInput
+                  {...input}
+                  variant="outlined"
+                  margin="normal"
+                  required
+                  fullWidth
+                  name="confirm password"
+                  label={t.register.confirmPassword}
+                  id="password2"
+                />
+              )}
+            />
+
+            <br />
+            <Button type="submit" fullWidth variant="contained" color="primary" size="large">
+              {t.register.submit}
+            </Button>
+            <br />
+            <Link to={ROUTES.LOGIN}>{t.register.linkToLogin}</Link>
+          </Paper>
+        </form>
+      )}
+    />
   </Container>
 ))
