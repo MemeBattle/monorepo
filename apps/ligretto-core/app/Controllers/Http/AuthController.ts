011d23a3b3d1f6eab469dc31c26841f5c2f16a46
Artem Zverev <themezv@gmail.com>
2021-01-24 03:38:17 +0700

    Add user model

--- apps/ligretto-core/app/Controllers/Http/AuthController.ts
+++ apps/ligretto-core/app/Controllers/Http/AuthController.ts
@@ -1,18 +1,34 @@
 import type { HttpContextContract } from '@ioc:Adonis/Core/HttpContext'
 import { login, signUp } from '@ioc:CasServices'
+import Logger from '@ioc:Adonis/Core/Logger'
+import UserModel from '../../Models/User'
 import LoginValidator from '../../Validators/LoginValidator'
 import SignUpValidator from '../../Validators/SignUpValidator'
 
 export default class AuthController {
-  async login({ request }: HttpContextContract) {
+  async login({ request, response }: HttpContextContract) {
     const data = await request.validate(LoginValidator)
     const loginResult = await login(data)
-    return loginResult
+    if(loginResult.success) {
+      const user = await UserModel.findBy('casId', loginResult.data.user._id)
+      if(!user) {
+        return response.badRequest()
+      }
+      return {...loginResult.data, profile: user}
+    }
+    return {...loginResult}
   }
 
-  async signUp({ request }: HttpContextContract) {
+  async signUp({ request, response }: HttpContextContract) {
     const data = await request.validate(SignUpValidator)
     const signUpResult = await signUp({ username: data.email, email: data.email, password: data.password })
-    return signUpResult
+    if(signUpResult.success) {
+      Logger.info(signUpResult.data._id)
+      const user = await UserModel.firstOrCreate({casId: signUpResult.data._id}, { casId: signUpResult.data._id })
+      return user
+    } else {
+      Logger.info(signUpResult.error.errorMessage)
+      return response.badRequest(signUpResult.error)
+    }
   }
 }
---
+++ apps/ligretto-core/app/Models/User.ts
@@ -0,0 +1,22 @@
+import type { DateTime } from 'luxon'
+import { BaseModel, column } from '@ioc:Adonis/Lucid/Orm'
+
+export default class User extends BaseModel {
+  public static table = 'users'
+
+  public static selfAssignPrimaryKey = true
+
+  public static connection = 'pg'
+
+  @column({ isPrimary: true })
+  public id: number
+
+  @column.dateTime({ autoCreate: true })
+  public createdAt: DateTime
+
+  @column.dateTime({ autoCreate: true, autoUpdate: true })
+  public updatedAt: DateTime
+
+  @column({columnName: 'casId'})
+  public casId: string
+}
