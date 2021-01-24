b9c457530f67302cf33a2d4574fbf998eedcf0ab
Artem Zverev <themezv@gmail.com>
2021-01-24 23:15:56 +0700

    Add memebattle logo

---
+++ packages/ligretto-ui/src/MemebattleLogo/MemebattleLogo.stories.tsx
@@ -0,0 +1,8 @@
+import React from 'react'
+import { MemebattleLogo } from './MemebattleLogo'
+
+export default {
+  title: 'MemebattleLogo',
+}
+
+export const DefaultView = () => <MemebattleLogo />
