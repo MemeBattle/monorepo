5f79257f2cca94149035d4a19d3cf067f966fafe
Artem Zverev <themezv@gmail.com>
2021-01-25 01:00:22 +0700

    add confirm email page

--- apps/auth-front/src/App.tsx
+++ apps/auth-front/src/App.tsx
@@ -3,6 +3,7 @@ import { BrowserRouter, Switch, Route, Redirect } from 'react-router-dom'
 import { ROUTES } from './constants/routes'
 import { LoginPage } from './pages/LoginPage'
 import { RegisterPage } from './pages/RegisterPage'
+import { ConfirmEmailPage } from './pages/ConfirmEmailPage'
 
 import './styles.css'
 
@@ -15,6 +16,9 @@ function App() {
         </Route>
         <Route path={ROUTES.REGISTER} exact>
           <RegisterPage />
+        </Route>
+        <Route path={ROUTES.CONFIRM_EMAIL} exact>
+          <ConfirmEmailPage />
         </Route>
         <Redirect to={ROUTES.LOGIN} />
       </Switch>
--- apps/auth-front/src/utils/i18n.ts
+++ apps/auth-front/src/utils/i18n.ts
@@ -18,4 +18,12 @@ export const t = {
     text: 'Created by:',
   },
   header: 'Ligretto',
-}
+  confirmEmail: {
+    header: 'Confirm your email address',
+    message: ({ email }: { email: string }) =>
+      `We have sent an email with a confirmation link to ${email}. In order to complete the sign-up process, please click the confirmation link.
+      If you do not receive a confirmation email, please check your spam folder.
+      Also, please verify that you entered a valid email address in our sign-up form.
+      If you need assistance, please contact us.`,
+  },
+} as const
