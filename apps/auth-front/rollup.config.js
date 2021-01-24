232f975e3ce1df74d2e2c30d170f0614a263b4a3
Artem Zverev <themezv@gmail.com>
2021-01-25 02:27:46 +0700

    Add missed rollup config

---
+++ apps/auth-front/rollup.config.js
@@ -0,0 +1,30 @@
+import resolve from '@rollup/plugin-node-resolve'
+import peerDepsExternal from 'rollup-plugin-peer-deps-external'
+import typescript from 'rollup-plugin-typescript2'
+import commonjs from '@rollup/plugin-commonjs'
+import scss from 'rollup-plugin-scss'
+// import url from '@rollup/plugin-url'
+
+export default {
+  input: 'src/module.ts',
+  output: [
+    {
+      file: 'dist/module.js',
+      format: 'cjs',
+      sourcemap: true,
+    },
+    {
+      file: 'dist/module.esm.js',
+      format: 'esm',
+      sourcemap: true,
+    },
+  ],
+  plugins: [
+    peerDepsExternal(),
+    resolve(),
+    commonjs(),
+    scss(),
+    // url(),
+    typescript(),
+  ],
+}
