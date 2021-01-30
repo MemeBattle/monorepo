0005164425c106fb63579bb1acfa6696e2d25398
Artem Zverev <artem.zverev@abbyy.com>
2021-01-30 22:28:19 +0700

    improve UserInfo component

--- packages/ligretto-ui/src/UserInfo/UserInfo.tsx
+++ packages/ligretto-ui/src/UserInfo/UserInfo.tsx
@@ -1,5 +1,7 @@
+import type { MouseEventHandler } from 'react'
 import React, { useMemo } from 'react'
 import { createStyles, makeStyles, Button } from '@material-ui/core'
+import ExitToApp from '@material-ui/icons/ExitToApp'
 import user1 from './assets/u1.svg'
 import user2 from './assets/u2.svg'
 import user3 from './assets/u3.svg'
@@ -14,7 +16,7 @@ const userAvatars = [user1, user2, user3, user4, user5, user6, user7, user8]
 
 const getRandomAvatar = () => userAvatars[Math.floor(Math.random() * userAvatars.length)]
 
-const useStyles = makeStyles(
+const useStyles = makeStyles(theme =>
   createStyles({
     mainUser: {
       display: 'flex',
@@ -27,19 +29,41 @@ const useStyles = makeStyles(
       maxHeight: '12rem',
       display: 'flex',
       cursor: 'pointer',
+      position: 'relative',
+      borderRadius: '0.8rem',
+      overflow: 'hidden',
     },
     image: {
+      padding: '2px',
       maxWidth: '100%',
       maxHeight: '100%',
     },
+    signupButton: {
+      position: 'absolute',
+      height: '100%',
+      width: '100%',
+      display: 'flex',
+      alignItems: 'center',
+      justifyContent: 'center',
+    },
+    button: {
+      border: 'solid 2px white',
+    },
+    backdrop: {
+      width: '100%',
+      height: '100%',
+      background: theme.palette.grey.A700,
+      opacity: '40%',
+      position: 'absolute',
+    },
     bottom: {
-      marginTop: '1rem',
+      marginTop: '0.5rem',
       display: 'flex',
       justifyContent: 'center',
     },
     username: {
       fontWeight: 'bold',
-      fontSize: '1.5rem',
+      fontSize: '1.6rem',
       color: 'white',
     },
   }),
@@ -47,8 +71,8 @@ const useStyles = makeStyles(
 
 export interface UserInfoProps {
   img?: string
-  onClick?: () => void
-  onButtonClick?: () => void
+  onClick?: MouseEventHandler
+  onButtonClick?: MouseEventHandler
   buttonText?: string
   username?: string
 }
@@ -58,22 +82,33 @@ export const UserInfo: React.FC<UserInfoProps> = ({ img, onClick, username, onBu
 
   const imageSrc = useMemo(() => img || getRandomAvatar(), [img])
 
+  const handleButtonClick: MouseEventHandler = e => {
+    e.stopPropagation()
+    if (onButtonClick) {
+      onButtonClick(e)
+    }
+  }
+
   return (
     <div className={classes.mainUser}>
       <div className={classes.mainUserAvatar} onClick={onClick}>
         <img className={classes.image} src={imageSrc} alt={username} />
+        {username ? null : (
+          <div className={classes.signupButton}>
+            <div className={classes.backdrop} />
+            <Button className={classes.button} color="primary" variant="contained" size="large" onClick={handleButtonClick} endIcon={<ExitToApp />}>
+              {buttonText}
+            </Button>
+          </div>
+        )}
       </div>
-      <div className={classes.bottom}>
-        {username ? (
+      {username ? (
+        <div className={classes.bottom}>
           <Typography className={classes.username} align="center">
             {username}
           </Typography>
-        ) : (
-          <Button variant="contained" size="medium" onClick={onButtonClick}>
-            {buttonText}
-          </Button>
-        )}
-      </div>
+        </div>
+      ) : null}
     </div>
   )
 }
