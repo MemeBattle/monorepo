775d49ce1eef6f8f05f984c8e793736cb9d5252f
Artem Zverev <artem.zverev@abbyy.com>
2021-01-31 03:35:46 +0700

    Add cas middleware and service for update user profile

--- apps/ligretto-core/app/Controllers/Http/UsersController.ts
+++ apps/ligretto-core/app/Controllers/Http/UsersController.ts
@@ -1,7 +1,14 @@
-// import { HttpContextContract } from '@ioc:Adonis/Core/HttpContext'
+import type { HttpContextContract } from '@ioc:Adonis/Core/HttpContext'
+import UpdateUserValidator from '../../Validators/UpdateUserValidator'
 
 export default class UsersController {
   public async index() {
     return [{ id: 1, username: 'themezv' }]
+  }
+
+  public async update({ request }: HttpContextContract) {
+    const data = await request.validate(UpdateUserValidator)
+    console.log('update', data)
+    return {}
   }
 }
