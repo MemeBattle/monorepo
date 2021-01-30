a6561a5dba0e649418594db6ea8fd62e6bd5e655
Artem Zverev <artem.zverev@abbyy.com>
2021-01-30 22:28:19 +0700

    improve UserInfo component

--- packages/ligretto-ui/src/UserInfo/UserInfo.stories.tsx
+++ packages/ligretto-ui/src/UserInfo/UserInfo.stories.tsx
@@ -1,21 +1,31 @@
 import React from 'react'
 import { UserInfo } from './UserInfo'
+import { Background } from '../story-components'
 
 export default {
   title: 'MainUser',
   argTypes: {
     onClick: { action: 'clicked' },
-    buttonText: { control: 'text', defaultValue: 'sign up' },
+    buttonText: { control: 'text', defaultValue: 'sign in' },
+    onButtonClick: { action: 'buttonClicked' },
   },
 }
 
-export const DefaultView = args => <UserInfo {...args} />
+export const DefaultView = args => (
+  <Background>
+    <UserInfo {...args} />
+  </Background>
+)
 
-export const Authorized = args => <UserInfo {...args} />
+export const Authorized = args => (
+  <Background>
+    <UserInfo {...args} />
+  </Background>
+)
 
 Authorized.argTypes = {
   onClick: { action: 'clicked' },
   username: { control: 'text', defaultValue: 'themezv' },
-  buttonText: { control: 'text', defaultValue: 'sign up' },
+  buttonText: { control: 'text', defaultValue: 'sign in' },
   onButtonClick: { action: 'button clicked' },
 }
