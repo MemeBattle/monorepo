279de309136281f2530d470a2215952921d9b733
Artem Zverev <artem.zverev@abbyy.com>
2021-03-18 00:25:11 +0300

    Remove node-sass and node-gyp from dependencies. Try CI with http npm registries

--- Dockerfile
+++ Dockerfile
@@ -4,5 +4,7 @@ COPY ./ /memebattle
 WORKDIR /memebattle
 
 RUN apk add --no-cache git
+RUN npm config set registry "http://registry.npmjs.org"
+RUN yarn config set registry "http://registry.npmjs.org"
 RUN yarn --network-timeout 100000
 RUN yarn run common-packages:build
--- apps/auth-front/rollup.config.js
+++ apps/auth-front/rollup.config.js
@@ -2,7 +2,6 @@ import resolve from '@rollup/plugin-node-resolve'
 import peerDepsExternal from 'rollup-plugin-peer-deps-external'
 import typescript from 'rollup-plugin-typescript2'
 import commonjs from '@rollup/plugin-commonjs'
-import scss from 'rollup-plugin-scss'
 import postCss from 'rollup-plugin-postcss-modules'
 
 export default {
@@ -23,7 +22,6 @@ export default {
     peerDepsExternal(),
     resolve({preferBuiltins: true, browser: true}),
     commonjs(),
-    // scss(),
     postCss(),
     typescript(),
   ],
