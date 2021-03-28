d81b822b68d75770a6c9c606f58efedb6708879a
Artem Zverev <artem.zverev@abbyy.com>
2021-03-28 03:06:19 +0300

    [CI]: add env variables to deploy

--- apps/ligretto-backend/src/config.ts
+++ apps/ligretto-backend/src/config.ts
@@ -1,7 +1,10 @@
 import { readFileSync } from 'fs'
 import { resolve } from 'path'
 
-export const { SOCKET_PORT, PARTNER_ID, CAS_URL } = process.env
+export const { SOCKET_PORT, PARTNER_ID, CAS_URL, LIGRETTO_CAS_KEY_PATH } = process.env
 
 export const SOCKET_ROOM_LOBBY = 'SOCKET_ROOM_LOBBY'
-export const PUBLIC_KEY = readFileSync(resolve(__dirname, '../key.pem')).toString()
+
+const PATH_TO_KEY = LIGRETTO_CAS_KEY_PATH || resolve(__dirname, '../key.pem')
+
+export const PUBLIC_KEY = readFileSync(PATH_TO_KEY).toString()
