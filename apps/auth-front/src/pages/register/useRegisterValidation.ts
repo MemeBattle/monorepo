62996babe0d550966f1a104fffb9227a18629e39
Vitaly Bibik <wital2010@gmail.com>
2021-04-04 17:49:08 +0300

    Add constants

--- apps/auth-front/src/pages/register/useRegisterValidation.ts
+++ apps/auth-front/src/pages/register/useRegisterValidation.ts
@@ -5,16 +5,20 @@ import { t } from '../../utils/i18n'
 
 export const useRegisterValidation = () =>
   useCallback((values: RegisterFormValues): RegisterFormValidationErrors => {
+    const USERNAME_MIN_LENGTH = 2
+    const USERNAME_MAX_LENGTH = 20
+    const PASSWORD_MIN_LENGTH = 8
+
     const errors: RegisterFormValidationErrors = {}
     if (!validateEmail(values.email)) {
       errors.email = t.validation.email
     }
 
-    if (!(values.username.trim().length >= 2 && values.username.trim().length <= 20)) {
+    if (!(values.username.trim().length >= USERNAME_MIN_LENGTH && values.username.trim().length <= USERNAME_MAX_LENGTH)) {
       errors.username = t.validation.username
     }
 
-    if (values.password.trim().length < 8) {
+    if (values.password.trim().length < PASSWORD_MIN_LENGTH) {
       errors.password = t.validation.password
     } else if (values.password !== values.confirmPassword) {
       errors.confirmPassword = t.validation.confirmPassword
