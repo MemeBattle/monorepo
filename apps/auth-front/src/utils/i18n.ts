5d4a0c4e46e96d0ca21d32984c7dcbd7a5e17799
Artem Zverev <themezv@gmail.com>
2021-05-04 17:39:02 +0300

    use updateUserProfileService (#19)
    * use updateUserProfileService
    
    * update cas-services
    
    * redirect on success signup
    
    Co-authored-by: Artem Zverev <themezv@gmail.com>

--- apps/auth-front/src/utils/i18n.ts
+++ apps/auth-front/src/utils/i18n.ts
@@ -26,8 +26,10 @@ export const t = {
   header: 'Ligretto',
   confirmEmail: {
     header: 'Confirm your email address',
-    message: ({ email }: { email: string }) =>
-      `We have sent an email with a confirmation link to ${email}. In order to complete the sign-up process, please click the confirmation link.
+    message: ({ email }: { email: string | null }) =>
+      `We have sent an email with a confirmation link to ${
+        email || 'your email'
+      }. In order to complete the sign-up process, please click the confirmation link.
       If you do not receive a confirmation email, please check your spam folder.
       Also, please verify that you entered a valid email address in our sign-up form.
       If you need assistance, please contact us.`,
