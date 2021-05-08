25bf2b80bee4469576191e6419ba28ced2d81d48
Artem Zverev <artem.zverev@abbyy.com>
2021-05-09 00:21:40 +0300

    Confirm email update UI

--- apps/auth-front/src/pages/confirm/ConfirmEmailPage.tsx
+++ apps/auth-front/src/pages/confirm/ConfirmEmailPage.tsx
@@ -1,21 +1,54 @@
-import { Container, Typography } from '@memebattle/ligretto-ui'
+import { Container, Typography, EmailSended, Box } from '@memebattle/ligretto-ui'
 
 import { t } from '../../utils/i18n'
 import { useLocation } from 'react-router'
 import { useMemo } from 'react'
+import { Header } from '../../components/Header'
 
 export const ConfirmEmailPage = () => {
   const { search } = useLocation()
 
-  const { email } = useMemo<{ email: string | null; username: string | null }>(() => {
+  const { email, username } = useMemo<{ email: string | null; username: string | null }>(() => {
     const searchParams = new URLSearchParams(search)
     return { email: searchParams.get('email'), username: searchParams.get('username') }
   }, [search])
 
   return (
-    <Container component="main" maxWidth="xs">
-      <Typography variant="h2">{t.confirmEmail.header}</Typography>
-      <Typography paragraph>{t.confirmEmail.message({ email })}</Typography>
+    <Container component="main" maxWidth="md">
+      <Header />
+      <Box m={2} justifyContent="center" display="flex">
+        <EmailSended />
+      </Box>
+      <Box m={2}>
+        <Typography align="center" variant="h3">
+          {t.confirmEmail.header}
+          {username ? (
+            <Typography variant="inherit" color="primary">
+              {' '}
+              {username}&nbsp;
+            </Typography>
+          ) : (
+            ''
+          )}
+          !
+        </Typography>
+      </Box>
+      <Box m={1}>
+        <Typography align="center" paragraph>
+          {t.confirmEmail.message}
+          {email ? (
+            <Typography variant="inherit" color="primary">
+              <b>{email}</b>
+            </Typography>
+          ) : (
+            'email'
+          )}
+          . {t.confirmEmail.messageEnd}
+        </Typography>
+      </Box>
+      <Typography align="center" color="textSecondary" variant="caption" paragraph>
+        {t.confirmEmail.submessage}
+      </Typography>
     </Container>
   )
 }
---
+++ packages/ligretto-ui/src/EmailSended/index.ts
@@ -0,0 +1,1 @@
+export { EmailSended } from './EmailSended'
