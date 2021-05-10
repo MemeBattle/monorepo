ac27d25ef7ebbeb824304ee6d13206f6d3177fd5
Vitaly Bibik <wital2010@gmail.com>
2021-05-10 21:30:04 +0300

    Rename components

--- apps/auth-front/src/components/AvatarDropZone/AvatarDropZone.tsx
+++ apps/auth-front/src/components/AvatarDropZone/AvatarDropZone.tsx
@@ -2,7 +2,7 @@ import React, { useEffect, useState } from 'react'
 import { useDropzone } from 'react-dropzone'
 import styles from './AvatarDropZone.module.scss'
 import { ReactComponent as AvatarPlaceHolder } from '../../images/UserPhoto.svg'
-import { ReactComponent as ButtonLogo } from '../../images/UploadButton.svg'
+import { ReactComponent as UploadIcon } from '../../images/UploadButton.svg'
 import { Button } from '@memebattle/ligretto-ui'
 import cn from 'classnames'
 import { DropBox } from '../DropBox/DropBox'
@@ -48,7 +48,7 @@ export const AvatarDropZone = (props: DropZoneProps) => {
         <div className={cn(styles.container, styles.container__border)}>
           {file ? <UserPhotoDrop file={file} /> : <AvatarPlaceHolder className={styles.container__img} />}
           <div className={styles.buttonWrapper}>
-            <Button size="medium" variant="outlined" color="inherit" endIcon={<ButtonLogo>send</ButtonLogo>}>
+            <Button size="medium" variant="outlined" color="inherit" endIcon={<UploadIcon>send</UploadIcon>}>
               {t.avatarDropZone.text}
             </Button>
           </div>
--- apps/auth-front/src/components/DropBox/DropBox.tsx
+++ apps/auth-front/src/components/DropBox/DropBox.tsx
@@ -1,12 +1,12 @@
 import React from 'react'
 import styles from './DropBox.module.scss'
-import { ReactComponent as DropLogo } from '../../images/DropImage.svg'
+import { ReactComponent as DropIcon } from '../../images/DropImage.svg'
 import { Typography } from '@memebattle/ligretto-ui'
 import { t } from '../../utils/i18n'
 
 export const DropBox = () => (
   <div className={styles.box}>
-    <DropLogo className={styles.box__logo} />
+    <DropIcon className={styles.box__logo} />
     <Typography variant="subtitle1" align="center" gutterBottom color="inherit">
       {t.dropBox}
     </Typography>
