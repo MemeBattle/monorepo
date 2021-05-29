acea2a445414884d63b8169c2ca29ce2247ba7ee
Artem Zverev <themezv@gmail.com>
2021-05-29 23:07:51 +0300

    Deploy ligretto core (#28)
    * Update Adonis and fix ts config
    
    * deploy ligretto-core
    
    Co-authored-by: Artem Zverev <artem.zverev@abbyy.com>

--- apps/ligretto-core/contracts/env.ts
+++ apps/ligretto-core/contracts/env.ts
@@ -19,5 +19,7 @@ declare module '@ioc:Adonis/Core/Env' {
   */
 
   type CustomTypes = typeof import('../env').default
-  type EnvTypes = CustomTypes
+  interface EnvTypes extends CustomTypes {
+    qwe: string
+  }
 }
