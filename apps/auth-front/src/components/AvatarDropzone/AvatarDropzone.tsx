7b5e1f5c159061b1fb2a0deb0bb3d546f70132cf
Artem Zverev <themezv@gmail.com>
2021-06-29 00:01:11 +0300

    Show profile avatar. Add Alert and Snackbar (#35)
    * Show profile avatar. Add Alert and Snackbar
    
    * Move snackbar alert into AvatarDropzone
    
    Co-authored-by: Artem Zverev <themezv@gmail.com>

--- apps/auth-front/src/components/AvatarDropzone/AvatarDropzone.tsx
+++ apps/auth-front/src/components/AvatarDropzone/AvatarDropzone.tsx
@@ -1,31 +1,49 @@
-import React, { useEffect, useState } from 'react'
+import React, { memo, useEffect, useState } from 'react'
 import { useDropzone } from 'react-dropzone'
 import styles from './AvatarDropzone.module.scss'
 import { ReactComponent as AvatarPlaceHolder } from '../../images/UserPhoto.svg'
 import { ReactComponent as UploadIcon } from '../../images/UploadButton.svg'
-import { Button } from '@memebattle/ligretto-ui'
+import { Alert, Button, Snackbar } from '@memebattle/ligretto-ui'
 import cn from 'classnames'
 import { DropBox } from '../DropBox'
 import { UserPhotoDrop } from '../UserPhotoDrop'
 import { t } from '../../utils/i18n'
+import { useUploadError } from '../../hooks/useUploadError'
 
 interface DropZoneProps {
   onChange?: (file: File) => void
+  avatarUrl?: string
 }
 
 type FilePreview = File & {
   preview: string
 }
 
-export const AvatarDropzone = (props: DropZoneProps) => {
-  const [file, setFile] = useState<FilePreview | null>(null)
+export const MAX_FILE_SIZE = 2097152 // 2mb
+
+export const AvatarDropzone = memo(({ avatarUrl, onChange }: DropZoneProps) => {
+  const [file, setFile] = useState<FilePreview | null>(() => {
+    if (avatarUrl) {
+      const file: FilePreview = { ...new File([], 'default'), preview: avatarUrl }
+      return file
+    }
+    return null
+  })
+
+  const [hasUploadError, handleError, clearError] = useUploadError()
+
   const { getRootProps, getInputProps, isDragActive } = useDropzone({
     accept: ['image/png', 'image/svg', 'image/jpeg', 'image/jpg'],
-    onDrop: acceptedFiles => {
+    maxSize: MAX_FILE_SIZE,
+    onDropAccepted: acceptedFiles => {
       const file = acceptedFiles[0]
       const fileWithPreview: FilePreview = { ...file, preview: URL.createObjectURL(file) }
       setFile(fileWithPreview)
-      props.onChange?.(file)
+      clearError()
+      onChange?.(file)
+    },
+    onDropRejected: fileRejections => {
+      handleError(fileRejections[0])
     },
   })
 
@@ -40,20 +58,25 @@ export const AvatarDropzone = (props: DropZoneProps) => {
   )
 
   return (
-    <div className={styles.dropZone} {...getRootProps()}>
-      <input {...getInputProps()} />
-      {!isDragActive ? (
-        <div className={cn(styles.container, styles.containerBorder)}>
-          {file ? <UserPhotoDrop file={file} /> : <AvatarPlaceHolder className={styles.containerImg} />}
-          <div className={styles.buttonWrapper}>
-            <Button size="medium" variant="outlined" color="inherit" endIcon={<UploadIcon>send</UploadIcon>}>
-              {t.avatarDropZone.text}
-            </Button>
+    <>
+      <div className={styles.dropZone} {...getRootProps()}>
+        <input {...getInputProps()} />
+        {!isDragActive ? (
+          <div className={cn(styles.container, styles.containerBorder, { [styles.containerBorderError]: hasUploadError })}>
+            {file ? <UserPhotoDrop file={file} /> : <AvatarPlaceHolder className={styles.containerImg} />}
+            <div className={styles.buttonWrapper}>
+              <Button size="medium" variant="outlined" color="inherit" endIcon={<UploadIcon>send</UploadIcon>}>
+                {t.avatarDropZone.text}
+              </Button>
+            </div>
           </div>
-        </div>
-      ) : (
-        <DropBox />
-      )}
-    </div>
+        ) : (
+          <DropBox />
+        )}
+      </div>
+      <Snackbar open={hasUploadError} anchorOrigin={{ vertical: 'top', horizontal: 'center' }}>
+        <Alert severity="error">{t.profile.maxFileSizeError}</Alert>
+      </Snackbar>
+    </>
   )
-}
+})
--- apps/auth-front/src/module.tsx
+++ apps/auth-front/src/module.tsx
@@ -7,10 +7,17 @@ import { createFrontServices } from '@memebattle/cas-services'
 export interface AuthFrontModuleProps {
   onLoginSucceeded: ({ token }: { token: string }) => void
   partnerId: string
+  staticFilesUrl: string
 }
 
-export const AuthFrontModule: FC<AuthFrontModuleProps> = ({ partnerId, onLoginSucceeded }) => {
-  const casServices = useMemo(() => createFrontServices({ partnerId, casURI: 'https://cas.mems.fun/api' }), [partnerId])
+export const AuthFrontModule: FC<AuthFrontModuleProps> = ({ partnerId, onLoginSucceeded, staticFilesUrl }) => {
+  const casServices = useMemo(
+    () => ({
+      ...createFrontServices({ partnerId, casURI: process.env.REACT_APP_CAS_URL || 'https://cas.mems.fun/api' }),
+      getAbsoluteUrl: (relativePath: string) => `${staticFilesUrl}${relativePath}`,
+    }),
+    [partnerId, staticFilesUrl],
+  )
 
   return (
     <CasServicesContext.Provider value={casServices}>
