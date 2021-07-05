317d671f8bd42ec8dc6ace13648a34a0c41cbf70
Artem Zverev <themezv@gmail.com>
2021-07-05 13:49:16 +0300

    Auto-login on init (#39)
    Co-authored-by: Artem Zverev <themezv@gmail.com>

--- apps/ligretto-recovery/src/ducks/auth/authSagas.ts
+++ apps/ligretto-recovery/src/ducks/auth/authSagas.ts
@@ -1,14 +1,23 @@
-import { getMeRequest } from './authActions'
-import { getMeSuccess } from './authActions'
+import { getMeRequest, getMeSuccess } from './authActions'
 import { call, put, takeLeading } from 'redux-saga/effects'
 import { getUserByTokenSaga } from 'ducks/users'
+import { LOCAL_STORAGE_TOKEN_KEY } from './constants'
+
+export function* initSaga() {
+  const token = window.localStorage.getItem(LOCAL_STORAGE_TOKEN_KEY)
+  if (token) {
+    yield put(getMeRequest({ token }))
+  }
+}
 
 export function* getMeSaga({ payload }: ReturnType<typeof getMeRequest>) {
   const userId: string = yield call(getUserByTokenSaga, payload.token)
 
+  window.localStorage.setItem(LOCAL_STORAGE_TOKEN_KEY, payload.token)
   yield put(getMeSuccess({ userId }))
 }
 
 export function* authRootSaga() {
   yield takeLeading(getMeRequest, getMeSaga)
+  yield call(initSaga)
 }
