b04d59cadbec32a5d2da210b7fbba44fd129da80
Artem Zverev <themezv@gmail.com>
2021-07-17 14:30:12 +0300

    RoomGrid refactoring. Change interface (#40)
    Co-authored-by: Artem Zverev <artem.zverev@instamart.ru>

--- apps/ligretto-recovery/src/containers/game/Game.tsx
+++ apps/ligretto-recovery/src/containers/game/Game.tsx
@@ -1,25 +1,11 @@
 import React from 'react'
 import { useSelector } from 'react-redux'
-import type { Player } from '@memebattle/ligretto-shared'
 import { selectPlaygroundDecks, selectOpponents } from 'ducks/game'
-import type { PositionOnTable } from '@memebattle/ligretto-ui'
-import { isMultiplyRenderChildren, RoomGrid } from '@memebattle/ligretto-ui'
+import { RoomGrid } from '@memebattle/ligretto-ui'
 import { OpponentCards } from 'components/blocks/game/opponent-cards'
 import { Playground } from 'components/blocks/game'
 import { CardsPanelContainer } from '../cards-panel'
 import { createSelector } from 'reselect'
-
-const createRenderChildren = (opponents: Player[]) => {
-  const renderChild = opponents.map(opponent => (positionOnTable: PositionOnTable) => (
-    <OpponentCards key={positionOnTable} positionOnTable={positionOnTable} cards={opponent.cards} stackOpenDeckCards={opponent.stackOpenDeck.cards} />
-  ))
-
-  if (isMultiplyRenderChildren(renderChild)) {
-    return renderChild
-  } else {
-    return null
-  }
-}
 
 const GameSelector = createSelector([selectOpponents, selectPlaygroundDecks], (opponents, cardsDecks) => ({
   opponents,
@@ -28,11 +14,14 @@ const GameSelector = createSelector([selectOpponents, selectPlaygroundDecks], (o
 
 export const Game = () => {
   const { opponents, cardsDecks } = useSelector(GameSelector)
-  const renderChildren = React.useMemo(() => createRenderChildren(opponents), [opponents])
 
   return (
     <>
-      {renderChildren ? <RoomGrid renderChildren={renderChildren} /> : null}
+      <RoomGrid>
+        {opponents.map(opponent => (
+          <OpponentCards key={opponent.id} cards={opponent.cards} stackOpenDeckCards={opponent.stackOpenDeck.cards} />
+        ))}
+      </RoomGrid>
       <Playground cardsDecks={cardsDecks} />
       <CardsPanelContainer />
     </>
