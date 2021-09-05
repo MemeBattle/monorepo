afa0117e9c9c42b403c5e8d9108e3637d117398e
Artem Zverev <artem.zverev@instamart.ru>
2021-09-05 17:49:21 +0300

    Add /users endpoint for ligretto-core

--- apps/ligretto-core/app/Controllers/Http/UsersController.ts
+++ apps/ligretto-core/app/Controllers/Http/UsersController.ts
@@ -1,7 +1,14 @@
 import { getUsers } from '@ioc:CasServices'
+import UsersListValidator from 'App/Validators/UsersListValidator'
+import type { HttpContextContract } from '@ioc:Adonis/Core/HttpContext'
+import { validator } from '@ioc:Adonis/Core/Validator'
 
 export default class UsersController {
-  public async index() {
-    return getUsers()
+  public async index(ctx: HttpContextContract) {
+    const { schema } = new UsersListValidator(ctx)
+
+    const { ids } = await validator.validate({ schema, data: ctx.request.qs() })
+
+    return getUsers({ ids })
   }
 }
