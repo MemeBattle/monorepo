ac9dcc2f272962030d9b01744c8f3b29982c20fc
BoldyrevEV <43893350+BoldyrevEV@users.noreply.github.com>
2021-09-18 00:27:13 +0300

    Moved gamehub-client to apps, and run lint:js:fix (#41)
    * Moved gamehub-client to apps, and run lint:js:fix
    
    * Moved gamehub-client to apps, and run lint:js:fix

--- apps/ligretto-backend/src/entities/player/player.repo.ts
+++ apps/ligretto-backend/src/entities/player/player.repo.ts
@@ -1,6 +1,6 @@
 import { inject, injectable } from 'inversify'
 import type { Card, CardsDeck } from '@memebattle/ligretto-shared'
-import type { Database } from '../../database'
+import { Database } from '../../database'
 import { IOC_TYPES } from '../../IOC_TYPES'
 
 @injectable()
--- apps/ligretto-backend/src/entities/user/user.service.ts
+++ apps/ligretto-backend/src/entities/user/user.service.ts
@@ -1,6 +1,6 @@
 import { inject, injectable } from 'inversify'
 import { IOC_TYPES } from '../../IOC_TYPES'
-import type { UserRepository } from './user.repo'
+import { UserRepository } from './user.repo'
 import type { User } from '../../types/user'
 
 @injectable()
