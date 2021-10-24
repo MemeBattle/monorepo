754e04b124c70421cbd6e5476c66833ad999616d
stepandyubin <svd18101997@gmail.com>
2021-10-24 14:30:01 +0300

    [LIG-82]: Game slice restructuring

--- apps/ligretto-recovery/src/ducks/game/selectors.ts
+++ apps/ligretto-recovery/src/ducks/game/selectors.ts
@@ -4,20 +4,29 @@ import type { Player } from '@memebattle/ligretto-shared'
 import type { All } from '../../types/store'
 import { selectCurrentUserId } from 'ducks/auth'
 
-export const selectGameId = (state: All) => state.game.id
-export const selectGameStatus = (state: All) => state.game.status
-export const selectPlayers = (state: All) => state.game.players
+export const selectGame = (state: All) => state.game.game
+export const selectGameId = (state: All) => selectGame(state).id
+export const selectGameStatus = (state: All) => selectGame(state).status
+export const selectPlayers = (state: All) => selectGame(state).players
 export const selectIsGameLoaded = (state: All) => state.game.isGameLoaded
 export const selectGameResults = (state: All) => state.game.results
-export const selectPlaygroundDecks = (state: All) => state.game.playground.decks
+export const selectPlaygroundDecks = (state: All) => selectGame(state).playground.decks
 
+/** Game Config */
+export const selectGameConfig = (state: All) => selectGame(state).config
+export const selectIsDndEnabled = (state: All) => selectGameConfig(state).dndEnabled
+
+/** Player State */
 export const selectPlayer = (state: All): Player => selectPlayers(state)[selectCurrentUserId(state)]
-
 export const selectPlayerCards = (state: All) => selectPlayer(state).cards
 export const selectPlayerStackOpenDeckCards = (state: All) => selectPlayer(state).stackOpenDeck.cards
 export const selectPlayerStackDeckCards = (state: All) => selectPlayer(state).stackDeck.cards
 export const selectPlayerLigrettoDeckCards = (state: All) => selectPlayer(state).ligrettoDeck.cards
 export const selectPlayerStatus = (state: All) => selectPlayer(state).status
+
+/** Local Player State */
+export const selectLocalPlayerState = (state: All) => state.game.localPlayerState
+export const selectSelectedCardIndex = (state: All) => selectLocalPlayerState(state).selectedCardIndex
 
 export const selectOpponents = createSelector([selectPlayers, selectCurrentUserId], (players, playerId = '') =>
   Object.values(omit(players, playerId)),
