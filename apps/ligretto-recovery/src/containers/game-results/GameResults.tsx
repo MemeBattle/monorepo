5f6c2152dae87d44080084e0f2a743938df4f3f1
Artem Zverev <themezv@gmail.com>
2021-10-30 22:42:05 +0300

    LIG-64 results table connect users

--- apps/ligretto-recovery/src/containers/game-results/GameResults.tsx
+++ apps/ligretto-recovery/src/containers/game-results/GameResults.tsx
@@ -1,20 +1,30 @@
 import React from 'react'
 import { useSelector } from 'react-redux'
 import { selectGameResults } from 'ducks/game'
+import { selectUsersMap } from 'ducks/users'
+import { selectCurrentUserId } from 'ducks/auth'
 import { ResultsTable } from '@memebattle/ligretto-ui'
 import { createSelector } from 'reselect'
 import sortBy from 'lodash/sortBy'
+import { buildCasStaticUrl } from 'utils/buildCasStaticUrl'
 
 const selectGameResultsForTable = createSelector(
   selectGameResults,
-  gameResults =>
+  selectUsersMap,
+  selectCurrentUserId,
+  (gameResults, users, currentPlayerId) =>
     gameResults &&
-    sortBy(Object.entries(gameResults), [([_, playerResults]) => -playerResults.gameScore]).map(([playerId, playerResults], index) => ({
-      position: index + 1,
-      username: playerId,
-      roundPoints: playerResults.roundScore,
-      totalPoints: playerResults.gameScore,
-    })),
+    sortBy(Object.entries(gameResults), [([_, playerResults]) => -playerResults.gameScore]).map(([playerId, playerResults], index) => {
+      const user = users[playerId]
+      return {
+        position: index + 1,
+        username: user?.isTemporary ? 'anon' : user?.username || '',
+        avatar: !user?.isTemporary && user?.avatar ? buildCasStaticUrl(user.avatar) : undefined,
+        roundPoints: playerResults.roundScore,
+        totalPoints: playerResults.gameScore,
+        isPlayer: user?.casId === currentPlayerId,
+      }
+    }),
 )
 
 export const GameResults = () => {
--- packages/ligretto-ui/src/ResultsTable/ResultsTable.stories.tsx
+++ packages/ligretto-ui/src/ResultsTable/ResultsTable.stories.tsx
@@ -7,8 +7,8 @@ export default {
 }
 
 const players = [
-  { position: 1, username: 'ThemeZV', roundPoints: 12, totalPoints: 22 },
-  { position: 2, username: 'ThemeZV2', roundPoints: -4, totalPoints: 20 },
+  { position: 1, username: 'ThemeZV2', roundPoints: -4, totalPoints: 20 },
+  { position: 2, username: 'ThemeZV', roundPoints: 12, totalPoints: 22, isPlayer: true },
   { position: 3, username: 'ThemeZV3', roundPoints: 0, totalPoints: 8 },
   { position: 4, username: 'ThemeZV4', roundPoints: 1, totalPoints: 2 },
 ]
