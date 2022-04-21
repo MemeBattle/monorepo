0aacf675073d2933386fbb07954d73c612a039f2
Vitaly Bibik <61142441+VitalyBibik@users.noreply.github.com>
2022-04-21 10:16:32 +0300

    [LIG-91]: Add Active Player styles in game (#94)
    * [LIG-91]: Add Active Player styles in game
    
    * [LIG-91]: Remove unused selector
    
    * [LIG-91]: Review
    
    * [LIG-91]: Fix interface in CardsPanel

--- apps/ligretto-recovery/src/containers/cards-panel/CardsPanelContainer.tsx
+++ apps/ligretto-recovery/src/containers/cards-panel/CardsPanelContainer.tsx
@@ -1,9 +1,9 @@
-import React, { useCallback } from 'react'
+import React, { useCallback, useMemo } from 'react'
 import { useDispatch, useSelector } from 'react-redux'
 import { createSelector } from 'reselect'
-
 import { CardsPanel } from 'components/blocks/game'
-import { tapLigrettoDeckCardAction, selectPlayerLigrettoDeckCards } from 'ducks/game'
+import { tapLigrettoDeckCardAction, selectPlayerLigrettoDeckCards, selectActivePlayer } from 'ducks/game'
+import { buildCasStaticUrl } from 'utils/buildCasStaticUrl'
 
 const CardsPanelContainerSelector = createSelector([selectPlayerLigrettoDeckCards], playerLigrettoDeckCards => ({
   playerLigrettoDeckCards,
@@ -11,11 +11,16 @@ const CardsPanelContainerSelector = createSelector([selectPlayerLigrettoDeckCard
 
 export const CardsPanelContainer = () => {
   const dispatch = useDispatch()
+  const player = useSelector(selectActivePlayer)
   const { playerLigrettoDeckCards } = useSelector(CardsPanelContainerSelector)
-
+  const playerWithStaticAvatar = useMemo(() => {
+    if (player) {
+      const avatar = player?.avatar ? buildCasStaticUrl(player.avatar) : undefined
+      return { ...player, avatar }
+    }
+  }, [player])
   const onLigrettoDeckCardClick = useCallback(() => {
     dispatch(tapLigrettoDeckCardAction())
   }, [dispatch])
-
-  return <CardsPanel ligrettoDeckCards={playerLigrettoDeckCards} onLigrettoDeckCardClick={onLigrettoDeckCardClick} />
+  return <CardsPanel ligrettoDeckCards={playerLigrettoDeckCards} onLigrettoDeckCardClick={onLigrettoDeckCardClick} player={playerWithStaticAvatar} />
 }
