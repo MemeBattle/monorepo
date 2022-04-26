97e929ee2bb1ebca3cd5b4d9b1b5e231f2309b53
APilishenko <pilishenkoa@gmail.com>
2022-04-27 00:06:05 +0300

    LIG-58 Refactoring rooms duck to use EntityAdapter

--- apps/ligretto-recovery/src/ducks/rooms/slice.ts
+++ apps/ligretto-recovery/src/ducks/rooms/slice.ts
@@ -1,23 +1,20 @@
 import type { Game, Room } from '@memebattle/ligretto-shared'
-import uniq from 'lodash/uniq'
-import omit from 'lodash/omit'
 import type { PayloadAction } from '@reduxjs/toolkit'
-import { createSlice, createAction } from '@reduxjs/toolkit'
+import { createSlice, createAction, createEntityAdapter } from '@reduxjs/toolkit'
 import type { CreateRoomError } from '@memebattle/ligretto-shared/src/dto'
 
-export type RoomsState = {
-  byId: {
-    [uuid: string]: Room
-  }
-  ids: string[]
+export const roomsEntityAdapter = createEntityAdapter<Room>({
+  selectId: room => room.uuid,
+})
+
+interface RoomsState extends ReturnType<typeof roomsEntityAdapter.getInitialState> {
   isLoading: boolean
   search: string
   error: CreateRoomError | null
 }
 
 const initialState: RoomsState = {
-  byId: {},
-  ids: [],
+  ...roomsEntityAdapter.getInitialState(),
   isLoading: false,
   search: '',
   error: null,
@@ -35,29 +32,14 @@ export const roomsSlice = createSlice({
       state.search = action.payload.search
     },
     updateRoomsAction: (state, action: PayloadAction<{ rooms: Room[] }>) => {
-      const { byId, ids } = action.payload.rooms.reduce<{ byId: { [roomId: string]: Room }; ids: string[] }>(
-        ({ byId, ids }, room) => ({ byId: { ...byId, [room.uuid]: room }, ids: [...ids, room.uuid] }),
-        {
-          byId: state.byId,
-          ids: state.ids,
-        },
-      )
-      return { ...state, byId, ids: uniq(ids) }
+      roomsEntityAdapter.upsertMany(state, action.payload.rooms)
     },
     setRoomsAction: (state, action: PayloadAction<{ rooms: Room[] }>) => {
-      const { byId: newById, ids: newIds } = action.payload.rooms.reduce<{ byId: { [roomId: string]: Room }; ids: string[] }>(
-        ({ byId, ids }, room) => ({ byId: { ...byId, [room.uuid]: room }, ids: [...ids, room.uuid] }),
-        {
-          byId: {},
-          ids: [],
-        },
-      )
-      return { ...state, byId: newById, ids: newIds, isLoading: false }
+      roomsEntityAdapter.setAll(state, action.payload.rooms)
+      state.isLoading = false
     },
-    removeRoomAction: (state, action: PayloadAction<{ uuid: Room['uuid'] }>): RoomsState => {
-      const newById = omit(state.byId, action.payload.uuid)
-      const newIds = state.ids.filter(id => id !== action.payload.uuid)
-      return { ...state, byId: newById, ids: newIds }
+    removeRoomAction: (state, action: PayloadAction<{ uuid: Room['uuid'] }>) => {
+      roomsEntityAdapter.removeOne(state, action.payload.uuid)
     },
     setErrorRoomsAction: (state, action: PayloadAction<{ error: CreateRoomError | null }>) => {
       state.error = action.payload.error
