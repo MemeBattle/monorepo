414870fe061104e311c1aac9115ecc0832bcf4dd
APilishenko <pilishenkoa@gmail.com>
2022-04-29 17:13:19 +0300

    LIG-58 Reset rooms list state for all after somebody leave game (#95)
    * LIG-58 Reset rooms list state for all after somebody leave game
    
    * LIG-58 Optimization: remove room instead of reset full rooms state
    
    * LIG-58 Refactoring rooms duck to use EntityAdapter

--- apps/ligretto-recovery/src/ducks/rooms/selectors.ts
+++ apps/ligretto-recovery/src/ducks/rooms/selectors.ts
@@ -1,7 +1,10 @@
 import type { All } from 'types/store'
+import { roomsEntityAdapter } from 'ducks/rooms/slice'
 
-export const selectRoomsList = (state: All) => state.rooms.ids.map(id => state.rooms.byId[id])
+const roomsEntitiesSelectors = roomsEntityAdapter.getSelectors<All>(state => state.rooms)
+
+export const selectRoomsList = roomsEntitiesSelectors.selectAll
 export const selectRoomsError = (state: All) => state.rooms.error
 export const selectIsLoading = (state: All) => state.rooms.isLoading
 export const selectSearch = (state: All) => state.rooms.search
-export const selectIsRoomsListEmpty = (state: All) => state.rooms.ids.length === 0
+export const selectIsRoomsListEmpty = (state: All) => roomsEntitiesSelectors.selectTotal(state) === 0
