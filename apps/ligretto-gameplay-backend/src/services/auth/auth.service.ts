2c7afff406e4b1d3ff1c7ed436a0c974fca3d6db
themezv <artem.zverev@peertown.com>
2022-06-11 17:42:21 +0400

    [LIG-95]: Fix after review

--- apps/ligretto-gameplay-backend/src/services/auth/auth.service.ts
+++ apps/ligretto-gameplay-backend/src/services/auth/auth.service.ts
@@ -1,57 +1,51 @@
 import { injectable } from 'inversify'
-import type { LoginCredentials, SignUpCredentials, SuccessLoginData, SuccessSignUpData } from '@memebattle/cas-services'
+import type { LoginCredentials, SignUpCredentials } from '@memebattle/cas-services'
 import { createCasServices } from '@memebattle/cas-services'
 import { CAS_URL, PARTNER_ID, PUBLIC_KEY } from '../../config'
 
 @injectable()
 export class AuthService {
-  public loginService: (credentials: LoginCredentials) => Promise<SuccessLoginData | null>
-  public signUpService: (credentials: SignUpCredentials) => Promise<SuccessSignUpData | null>
-  public verifyTokenService: (token: string) => Promise<{ userId: string } | null>
+  private casServices = createCasServices({ casURI: CAS_URL, partnerId: PARTNER_ID, publicKey: PUBLIC_KEY })
 
-  constructor() {
-    const { signUpService, loginService, verifyToken } = createCasServices({ casURI: CAS_URL, partnerId: PARTNER_ID, publicKey: PUBLIC_KEY })
-
-    this.loginService = async (credentials: LoginCredentials) => {
-      try {
-        const response = await loginService(credentials)
-        if (response.success) {
-          return response.data
-        } else {
-          return null
-        }
-      } catch (e) {
-        console.error(e)
+  public async loginService(credentials: LoginCredentials) {
+    try {
+      const response = await this.casServices.loginService(credentials)
+      if (response.success) {
+        return response.data
+      } else {
         return null
       }
+    } catch (e) {
+      console.error(e)
+      return null
     }
+  }
 
-    this.signUpService = async (credentials: SignUpCredentials) => {
-      try {
-        const response = await signUpService(credentials)
-        if (response.success) {
-          return response.data
-        } else {
-          return null
-        }
-      } catch (e) {
-        console.error(e)
+  public async signUpService(credentials: SignUpCredentials) {
+    try {
+      const response = await this.casServices.signUpService(credentials)
+      if (response.success) {
+        return response.data
+      } else {
         return null
       }
+    } catch (e) {
+      console.error(e)
+      return null
     }
+  }
 
-    this.verifyTokenService = async (token: string) => {
-      try {
-        const data = await verifyToken(token)
-        if (data.success) {
-          return { userId: data.data._id }
-        } else {
-          return null
-        }
-      } catch (e) {
-        console.error(e)
+  public async verifyTokenService(token: string) {
+    try {
+      const data = await this.casServices.verifyToken(token)
+      if (data.success) {
+        return { userId: data.data._id }
+      } else {
         return null
       }
+    } catch (e) {
+      console.error(e)
+      return null
     }
   }
 }
