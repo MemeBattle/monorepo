c43304751e2901c355c38075cc6ea7927d493831
themezv <artem.zverev@peertown.com>
2022-06-18 18:50:46 +0400

    LIG-126_useToken

--- apps/auth-front/src/hooks/useToken.ts
+++ apps/auth-front/src/hooks/useToken.ts
@@ -1,8 +1,9 @@
 import { useLocation } from 'react-router'
 import { useMemo } from 'react'
+import { LOCAL_STORAGE_TOKEN_KEY } from '@memebattle/ligretto-frontend/src/ducks/auth/constants'
 
 export const useToken = () => {
   const search = useLocation().search
 
-  return useMemo(() => new URLSearchParams(search).get('token'), [search])
+  return useMemo(() => new URLSearchParams(search).get('token') || window.localStorage.getItem(LOCAL_STORAGE_TOKEN_KEY), [search])
 }
