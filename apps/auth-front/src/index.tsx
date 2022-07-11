85559c202231a187f7d8152506fc1184b601b9b9
themezv <artem.zverev@peertown.com>
2022-07-12 02:35:45 +0400

    [LIG-132]: Fix typings

--- apps/auth-front/src/index.tsx
+++ apps/auth-front/src/index.tsx
@@ -1,7 +1,6 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
 import { App } from './App'
-import reportWebVitals from './reportWebVitals'
 
 import { ThemeProvider, theme, CssBaseline } from '@memebattle/ui'
 
@@ -21,8 +20,3 @@ if (rootNode?.hasChildNodes()) {
 } else {
   ReactDOM.render(AppJSX, rootNode)
 }
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals(console.log)
