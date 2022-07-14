4ea8f969ca96652fcacddac0731373419b8ef594
themezv <artem.zverev@peertown.com>
2022-07-15 02:19:05 +0400

    [LIG-128]: Add next-transpile-module and fix dockerfile

--- apps/gamehub-client/next.config.js
+++ apps/gamehub-client/next.config.js
@@ -1,6 +1,9 @@
+// eslint-disable-next-line @typescript-eslint/no-var-requires
+const withTM = require('next-transpile-modules')(['@memebattle/ui', '@memebattle/ligretto-shared'])
+
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
 }
 
-module.exports = nextConfig
+module.exports = withTM(nextConfig)
