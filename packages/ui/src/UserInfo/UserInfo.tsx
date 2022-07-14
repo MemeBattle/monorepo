fb4815ca6b9327422557c952a0272c0757ac4bf0
Vitaly Bibik <vitaliy.bibik@sbermarket.ru>
2022-07-14 19:58:55 +0300

    LIG-124-fixClearIcon: Fix closeIcon

--- packages/ui/src/UserInfo/UserInfo.tsx
+++ packages/ui/src/UserInfo/UserInfo.tsx
@@ -1,58 +1,74 @@
 import type { MouseEventHandler } from 'react'
+import { styled } from '@mui/material/styles'
 import React from 'react'
 import Button from '@mui/material/Button'
-import { createStyles, makeStyles } from '@mui/styles'
 import CreateIcon from '@mui/icons-material/Create'
 import ClearIcon from '@mui/icons-material/Clear'
 import Paper from '@mui/material/Paper'
 
-const useStyles = makeStyles(() =>
-  createStyles({
-    userInfo: {
-      maxWidth: '20rem',
-      position: 'relative',
-      width: '20rem',
-      height: '13rem',
-      display: 'flex',
-      alignItems: 'center',
-      flexDirection: 'column',
-    },
-    image: {
-      marginTop: '0.5rem',
-      maxWidth: '100%',
-      width: '9rem',
-      height: '9rem',
-    },
-    buttonField: {
-      height: '100%',
-      width: '100%',
-      display: 'flex',
-      justifyContent: 'center',
-    },
-    button: {
-      width: '70%',
-      height: '3rem',
-      padding: '0.5rem',
-      textTransform: 'none',
-      fontSize: '1.5rem',
-    },
-    clearIcon: {
-      position: 'absolute',
-      top: 0,
-      right: 0,
-      minWidth: 'auto',
-    },
-    createIcon: {
-      marginLeft: 0,
-    },
-    userNameField: {
-      textOverflow: 'ellipsis',
-      width: '10rem',
-      overflow: 'hidden',
-      marginLeft: '1.375rem',
-    },
-  }),
-)
+const PREFIX = 'UserInfo'
+
+const classes = {
+  userInfo: `${PREFIX}-userInfo`,
+  image: `${PREFIX}-image`,
+  buttonField: `${PREFIX}-buttonField`,
+  button: `${PREFIX}-button`,
+  clearIcon: `${PREFIX}-clearIcon`,
+  createIcon: `${PREFIX}-createIcon`,
+  userNameField: `${PREFIX}-userNameField`,
+}
+
+const StyledPaper = styled(Paper)(() => ({
+  [`& .${classes.userInfo}`]: {
+    maxWidth: '20rem',
+    position: 'relative',
+    width: '20rem',
+    height: '13rem',
+    display: 'flex',
+    alignItems: 'center',
+    flexDirection: 'column',
+  },
+
+  [`& .${classes.image}`]: {
+    marginTop: '0.5rem',
+    maxWidth: '100%',
+    width: '9rem',
+    height: '9rem',
+  },
+
+  [`& .${classes.buttonField}`]: {
+    height: '100%',
+    width: '100%',
+    display: 'flex',
+    justifyContent: 'center',
+  },
+
+  [`& .${classes.button}`]: {
+    width: '70%',
+    height: '3rem',
+    padding: '0.5rem',
+    textTransform: 'none',
+    fontSize: '1.5rem',
+  },
+
+  [`& .${classes.clearIcon}`]: {
+    position: 'absolute',
+    top: 0,
+    right: 0,
+    minWidth: 'auto',
+  },
+
+  [`& .${classes.createIcon}`]: {
+    marginLeft: 0,
+  },
+
+  [`& .${classes.userNameField}`]: {
+    textOverflow: 'ellipsis',
+    width: '10rem',
+    overflow: 'hidden',
+    marginLeft: '1.375rem',
+  },
+}))
 
 export interface UserInfoProps {
   img?: string
@@ -69,8 +85,6 @@ export interface UserInfoProps {
 }
 
 export const UserInfo: React.FC<UserInfoProps> = ({ onClick, username, onButtonClick, onLogoutClick, buttonText, children }) => {
-  const classes = useStyles()
-
   const handleButtonClick: MouseEventHandler = e => {
     e.stopPropagation()
 
@@ -88,7 +102,7 @@ export const UserInfo: React.FC<UserInfoProps> = ({ onClick, username, onButtonC
   }
 
   return (
-    <Paper>
+    <StyledPaper>
       <div className={classes.userInfo}>
         <div className={classes.image} onClick={onClick}>
           {children}
@@ -118,7 +132,7 @@ export const UserInfo: React.FC<UserInfoProps> = ({ onClick, username, onButtonC
           )}
         </div>
       </div>
-    </Paper>
+    </StyledPaper>
   )
 }
 
