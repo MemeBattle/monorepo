4cc3633650dea4addcf3e26f8b5f9307e0a77bf7
Artem Zverev <themezv@gmail.com>
2022-07-17 18:11:10 +0400

    [LIG-130]: Create gamehub layout (#111)
    * [LIG-130]: Create gamehub layout
    
    * [LIG-130]: Simple Header
    
    * [LIG-130]: Fix lint and tests
    
    Co-authored-by: Artem Zverev <themezv@gmail.com>

--- apps/gamehub-client/src/pages/index.tsx
+++ apps/gamehub-client/src/pages/index.tsx
@@ -1,10 +1,16 @@
-import type { NextPage } from 'next'
 import { Button } from '@memebattle/ui'
 
-const Home: NextPage = () => (
+import type { NextPageWithLayout } from '../types'
+
+import { MainLayout } from '../layouts/main'
+import type { ReactElement } from 'react'
+
+const Home: NextPageWithLayout = () => (
   <div>
     <Button>Hello!</Button>
   </div>
 )
+
+Home.getLayout = (page: ReactElement) => <MainLayout>{page}</MainLayout>
 
 export default Home
--- packages/ui/src/themes/gamehubClient/gamehubClientTheme.ts
+++ packages/ui/src/themes/gamehubClient/gamehubClientTheme.ts
@@ -2,8 +2,19 @@ import { createTheme } from '@mui/material/styles'
 
 export const gamehubClientTheme = createTheme({
   palette: {
+    mode: 'dark',
     background: {
       default: '#050D32',
+      light: '#212756',
+    },
+    primary: {
+      main: '#fff',
     },
   },
 })
+
+declare module '@mui/material/styles' {
+  interface TypeBackground {
+    light?: `#${string}`
+  }
+}
