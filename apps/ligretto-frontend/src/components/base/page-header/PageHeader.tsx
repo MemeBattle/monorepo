c5ea2ddc7ab46970568289e9098e5a53e372206d
Artem Zverev <themezv@gmail.com>
2022-08-08 13:38:31 +0400

    [LIG-170]: Remove unused components ligretto-frontend (#120)
    Co-authored-by: Artem Zverev <themezv@gmail.com>

--- apps/ligretto-frontend/src/components/base/page-header/PageHeader.tsx
+++
@@ -1,8 +0,0 @@
-import * as React from 'react'
-import cn from 'classnames'
-
-import styles from './PageHeader.module.scss'
-
-export const PageHeader: React.FC<{ className?: string }> = ({ className, children }) => (
-  <h1 className={cn(styles.pageHeader, className)}>{children}</h1>
-)
