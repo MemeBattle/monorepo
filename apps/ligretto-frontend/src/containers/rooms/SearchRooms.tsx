e5926c4c9d302f84ec9a79e150124b017f4c0816
Sergey Maksimov <s.mksimov@gmail.com>
2022-08-13 19:24:31 +0300

    [LIG-161]: refactor

--- apps/ligretto-frontend/src/containers/rooms/SearchRooms.tsx
+++ apps/ligretto-frontend/src/containers/rooms/SearchRooms.tsx
@@ -1,12 +1,11 @@
 import { useDispatch, useSelector } from 'react-redux'
 import React, { useCallback } from 'react'
 
-import type { SearchInputProps } from 'components/base/search-input'
 import { SearchInput } from 'components/base/search-input'
 import { selectIsLoading, selectSearch } from 'ducks/rooms/selectors'
 import { searchRoomsAction } from 'ducks/rooms'
 
-interface SearchRoomsContainerProps extends SearchInputProps {
+interface SearchRoomsContainerProps {
   className: string
 }
 
