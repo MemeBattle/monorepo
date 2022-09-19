870b1eb20975a758f92fd9fa91deceea40e8d934
themezv <artem.zverev@peertown.com>
2022-09-19 23:41:30 +0400

    [LIG-155]: Fix player sizes

--- apps/ligretto-frontend/src/components/blocks/game/Player/Player.tsx
+++ apps/ligretto-frontend/src/components/blocks/game/Player/Player.tsx
@@ -13,6 +13,20 @@ export interface PlayerProps {
   isActivePlayer?: boolean
 }
 
+const calcPlayerHeight = ({ status, isActivePlayer }: { status: PlayerStatus; isActivePlayer?: boolean }): string => {
+  switch (true) {
+    case isActivePlayer && status === PlayerStatus.InGame:
+      return '9rem'
+    case isActivePlayer:
+      return '12rem'
+    case !isActivePlayer && status === PlayerStatus.InGame:
+      return '3rem'
+    case !isActivePlayer:
+      return '10rem'
+  }
+  return '10rem'
+}
+
 const StyledPlayer = styled('div')<{ status: PlayerStatus; isActivePlayer?: boolean }>(({ status, isActivePlayer, theme }) => ({
   display: 'flex',
   flexDirection: status === PlayerStatus.InGame && !isActivePlayer ? 'row' : 'column',
@@ -20,6 +34,8 @@ const StyledPlayer = styled('div')<{ status: PlayerStatus; isActivePlayer?: bool
   [theme.breakpoints.down('sm')]: {
     maxWidth: '1.5rem',
   },
+  height: calcPlayerHeight({ isActivePlayer, status }),
+  width: isActivePlayer ? '12rem' : '10rem',
   alignItems: 'center',
   opacity: status === PlayerStatus.DontReadyToPlay ? 0.5 : 1,
   transition: 'opacity 100ms',
@@ -27,24 +43,23 @@ const StyledPlayer = styled('div')<{ status: PlayerStatus; isActivePlayer?: bool
 }))
 
 const StyledIconWrapper = styled('div')(() => ({
-  marginLeft: 'auto',
+  marginLeft: '0.5rem',
   fontSize: '2rem',
   width: '2rem',
   height: '2rem',
   lineHeight: '1',
 }))
 
-const Username = styled('span')<{ status: PlayerStatus }>(({ status }) => ({
+const Username = styled('span')<{ status: PlayerStatus; isActivePlayer?: boolean }>(({ status, isActivePlayer }) => ({
   color: '#fff',
-  fontSize: '1.5rem',
+  fontSize: status === PlayerStatus.InGame && !isActivePlayer ? '1rem' : '1.5rem',
   textAlign: 'center',
   textOverflow: 'ellipsis',
   overflow: 'hidden',
-  marginRight: status === PlayerStatus.InGame ? 0 : '0.5rem',
   width: '100%',
 }))
 
-const Bottom = styled('div')<{ status: PlayerStatus }>(({ theme, status }) => ({
+const Bottom = styled('div')<{ status: PlayerStatus; isActivePlayer?: boolean }>(({ theme, status, isActivePlayer }) => ({
   borderRadius: 4,
   background: theme.palette.primary.main,
   alignItems: 'center',
@@ -52,7 +67,7 @@ const Bottom = styled('div')<{ status: PlayerStatus }>(({ theme, status }) => ({
   padding: '0.5rem',
   maxWidth: '100%',
   width: '100%',
-  marginLeft: status === PlayerStatus.InGame ? '0.75rem' : 0,
+  marginLeft: status === PlayerStatus.InGame && !isActivePlayer ? '0.75rem' : 0,
   [theme.breakpoints.down('sm')]: {
     display: 'none',
   },
@@ -72,8 +87,8 @@ export const Player: React.FC<PlayerProps> = props => {
   return (
     <StyledPlayer status={status} isActivePlayer={isActivePlayer}>
       <Avatar src={avatar} alt={username} size="auto" />
-      <Bottom status={status}>
-        <Username status={status} title={username}>
+      <Bottom isActivePlayer={isActivePlayer} status={status}>
+        <Username isActivePlayer={isActivePlayer} status={status} title={username}>
           {username}
         </Username>
         {Icon ? (
