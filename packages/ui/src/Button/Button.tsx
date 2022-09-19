53d5d0969beb4e78140c3a47d45e5b886e6bb404
themezv <artem.zverev@peertown.com>
2022-09-19 20:31:31 +0400

    [LIG-194]: Fix UI components stories title prefix

--- packages/ui/src/Button/Button.tsx
+++
@@ -1,7 +0,0 @@
-import type { ButtonProps as MUIButtonProps } from '@mui/material'
-import { Button as MUIButton } from '@mui/material'
-import type { FC } from 'react'
-
-export type ButtonProps = MUIButtonProps
-
-export const Button: FC<ButtonProps> = MUIButton
--- packages/ui/src/Grid/Grid.tsx
+++
@@ -1,3 +0,0 @@
-import MUIGrid from '@mui/material/Grid'
-
-export const Grid = MUIGrid
