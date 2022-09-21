6e68edd9e3870b663b6ae926c2596f0344f675b2
Simon  Polyakov <s_Qin4ik@live.ru>
2022-09-21 16:37:44 +0400

    LIG-137 add describe

--- e2e/tests/game.spec.ts
+++ e2e/tests/game.spec.ts
@@ -3,38 +3,40 @@ import { GamePage } from '../support/pages/game'
 import { HomePage } from '../support/pages/home'
 import { random } from 'lodash'
 
-test('Two users enter one room', async () => {
-  const roomName = String(random(0, 10000))
-  const browser = await chromium.launch()
-  const contextUser1 = await browser.newContext()
-  const contextUser2 = await browser.newContext()
-  const pageUser1 = await contextUser1.newPage()
-  const pageUser2 = await contextUser2.newPage()
+test.describe('Create and enter room', () => {
+  test('Two users enter one room', async () => {
+    const roomName = String(random(0, 10000))
+    const browser = await chromium.launch()
+    const contextUser1 = await browser.newContext()
+    const contextUser2 = await browser.newContext()
+    const pageUser1 = await contextUser1.newPage()
+    const pageUser2 = await contextUser2.newPage()
 
-  const homePageUser1 = new HomePage(pageUser1)
-  await homePageUser1.visitHomeUrl()
+    const homePageUser1 = new HomePage(pageUser1)
+    await homePageUser1.visitHomeUrl()
 
-  /**
-   * User 1 create game
-   */
-  await (await homePageUser1.getMenuCreateGameButton()).click()
-  await (await homePageUser1.getRoomNameInput()).click()
-  await (await homePageUser1.getRoomNameInput()).fill(roomName)
-  await (await homePageUser1.getCreateGameButton()).click()
+    /**
+     * User 1 create game
+     */
+    await (await homePageUser1.getMenuCreateGameButton()).click()
+    await (await homePageUser1.getRoomNameInput()).click()
+    await (await homePageUser1.getRoomNameInput()).fill(roomName)
+    await (await homePageUser1.getCreateGameButton()).click()
 
-  const gamePageUser1 = new GamePage(pageUser1)
-  await expect(await gamePageUser1.getPlayerReadyButton()).toBeVisible()
+    const gamePageUser1 = new GamePage(pageUser1)
+    await expect(await gamePageUser1.getPlayerReadyButton()).toBeVisible()
 
-  /**
-   * User 2 enter created room
-   */
-  const homePageUser2 = new HomePage(pageUser2)
-  await homePageUser2.visitHomeUrl()
+    /**
+     * User 2 enter created room
+     */
+    const homePageUser2 = new HomePage(pageUser2)
+    await homePageUser2.visitHomeUrl()
 
-  await (await homePageUser2.getEnterRoomButton()).click()
-  await (await homePageUser2.getRoom(roomName)).click()
+    await (await homePageUser2.getEnterRoomButton()).click()
+    await (await homePageUser2.getRoom(roomName)).click()
 
-  const gamePageUser2 = new GamePage(pageUser2)
+    const gamePageUser2 = new GamePage(pageUser2)
 
-  await expect(await gamePageUser2.getOpponentWaiting()).toBeVisible()
-})
+    await expect(await gamePageUser2.getOpponentWaiting()).toBeVisible()
+  })
+}
