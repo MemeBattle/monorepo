b34510a6351348ee305bba19ba2282f62d5917c0
Artem Zverev <themezv@gmail.com>
2022-11-08 21:20:31 +0300

    [#198] add /api prefix to ligretto core (#232)
    [!198] add /api prefix to ligretto core

--- apps/ligretto-core-backend/start/routes.ts
+++ apps/ligretto-core-backend/start/routes.ts
@@ -21,15 +21,17 @@
 import Route from '@ioc:Adonis/Core/Route'
 import HealthCheck from '@ioc:Adonis/Core/HealthCheck'
 
-Route.get('/', async () => ({ hello: 'world' }))
+Route.group(() => {
+  Route.get('/', async () => ({ hello: 'world' }))
 
-Route.resource('users', 'UsersController').apiOnly()
+  Route.resource('users', 'UsersController').apiOnly()
 
-Route.post('/games', 'GamesController.create')
-Route.get('/games', 'GamesController.index')
-Route.post('/games/:id/rounds', 'GamesController.saveRound').where('id', Route.matchers.uuid())
+  Route.post('/games', 'GamesController.create')
+  Route.get('/games', 'GamesController.index')
+  Route.post('/games/:id/rounds', 'GamesController.saveRound').where('id', Route.matchers.uuid())
 
-Route.post('/auth/me', 'AuthController.me')
+  Route.post('/auth/me', 'AuthController.me')
+}).prefix('/api')
 
 Route.get('health', async ({ response }) => {
   const report = await HealthCheck.getReport()
