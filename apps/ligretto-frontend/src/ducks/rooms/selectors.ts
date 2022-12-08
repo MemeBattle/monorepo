f273afd5284b600b097b3e38abdc617a36c9a4cb
Konstantin Dyakov <konstantin.d@fora-soft.com>
2022-12-08 10:29:41 +0300

    [#229] memoize foundRoomSelector

--- apps/ligretto-frontend/src/ducks/rooms/selectors.ts
+++ apps/ligretto-frontend/src/ducks/rooms/selectors.ts
@@ -1,15 +1,14 @@
+import { createSelector } from '@reduxjs/toolkit'
 import type { All } from 'types/store'
 import { roomsEntityAdapter } from 'ducks/rooms/slice'
 
 const roomsEntitiesSelectors = roomsEntityAdapter.getSelectors<All>(state => state.rooms)
 
 export const roomsListSelector = roomsEntitiesSelectors.selectAll
-export const foundRoomsSelector = (state: All) => {
-  const rooms = roomsListSelector(state)
-  const search = state.rooms.search
-  return search ? rooms.filter(room => room.name.toLowerCase().includes(search.toLowerCase())) : rooms
-}
 export const roomsErrorSelector = (state: All) => state.rooms.error
 export const isLoadingSelector = (state: All) => state.rooms.isLoading
 export const searchSelector = (state: All) => state.rooms.search
 export const isRoomsListEmptySelector = (state: All) => roomsEntitiesSelectors.selectTotal(state) === 0
+export const foundRoomsSelector = createSelector(roomsListSelector, searchSelector, (roomsListSelector, searchSelector) =>
+  searchSelector ? roomsListSelector.filter(room => room.name.toLowerCase().includes(searchSelector.toLowerCase())) : roomsListSelector,
+)
