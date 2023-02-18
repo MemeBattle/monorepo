7c38a6c0a5cdd56d9577c6b77ee4cb18a7d198c1
Artem Zverev <themezv@gmail.com>
2023-02-18 18:17:38 +0400

    [262]: Use paper from mui (#263)

--- apps/ligretto-frontend/src/themes/default/theme.ts
+++ apps/ligretto-frontend/src/themes/default/theme.ts
@@ -4,5 +4,17 @@ import { palette } from './palette'
 export const theme = responsiveFontSizes(
   createTheme({
     palette,
+    components: {
+      MuiPaper: {
+        styleOverrides: {
+          root: {
+            boxShadow: '0px 0px 32px rgba(0, 0, 0, 0.15)',
+          },
+          rounded: {
+            borderRadius: '0.5rem',
+          },
+        },
+      },
+    },
   }),
 )
