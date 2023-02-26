ccfba4c27952fabe7872a185ff1daaa0797f2a43
Artem Zverev <themezv@gmail.com>
2023-02-26 17:27:28 +0400

    [#261]: Update gamehub-client nextjs to 13 and react to 18

--- apps/gamehub-client/next.config.js
+++ apps/gamehub-client/next.config.js
@@ -1,9 +1,9 @@
-// eslint-disable-next-line @typescript-eslint/no-var-requires
-const withTM = require('next-transpile-modules')(['@memebattle/ui'])
-
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  reactStrictMode: true,
+  transpilePackages: ['@memebattle/ui'],
+  experimental: {
+    appDir: true,
+  },
 }
 
-module.exports = withTM(nextConfig)
+module.exports = nextConfig
