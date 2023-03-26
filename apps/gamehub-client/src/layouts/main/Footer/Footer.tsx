012c3949e368d5ce2904075d01ec119e28fba025
Artem Zverev <themezv@gmail.com>
2023-03-26 15:27:16 +0400

    [#300]: gamehub-client migrate to app. Add favicon

--- apps/gamehub-client/src/layouts/main/Footer/Footer.tsx
+++ apps/gamehub-client/src/layouts/main/Footer/Footer.tsx
@@ -1,10 +1,19 @@
+'use client'
+import Image from 'next/image'
+
+import gitHubLogo from './GitHub_Logo_White.png'
 import { Box, useTheme, MemebattleLogo } from '@memebattle/ui'
 
 export const Footer = () => {
   const theme = useTheme()
 
   return (
-    <Box component="footer" display="flex" justifyContent="end" padding={theme.spacing(2)} height={theme.spacing(16)}>
+    <Box component="footer" display="flex" padding={theme.spacing(2)} height={theme.spacing(16)}>
+      <Box display="flex" alignItems="center" justifyContent="center" flex={1}>
+        <a href="https://github.com/MemeBattle/monorepo">
+          <Image width="180" alt="GitHub" src={gitHubLogo} />
+        </a>
+      </Box>
       <Box alignSelf="center" width={theme.spacing(10)}>
         <MemebattleLogo />
       </Box>
--- apps/gamehub-client/src/layouts/main/Header/Header.tsx
+++ apps/gamehub-client/src/layouts/main/Header/Header.tsx
@@ -1,17 +1,28 @@
+'use client'
+
 import { Button, Box, useTheme } from '@memebattle/ui'
+import type { LinkProps } from 'next/link'
+import Link from 'next/link'
 import type { FC, ReactNode } from 'react'
 
-const HeaderButton: FC<{ children: ReactNode }> = ({ children }) => <Button color="secondary">{children}</Button>
-
+const HeaderButton: FC<{ children: ReactNode; isDisabled?: boolean } & LinkProps> = ({ children, href, isDisabled }) => (
+  <Button component={Link} href={href as string} color="secondary" disabled={isDisabled}>
+    {children}
+  </Button>
+)
 export const Header = () => {
   const theme = useTheme()
 
   return (
     <Box component="header" display="flex" alignItems="center" justifyContent="center" height={theme.spacing(16)} maxHeight={theme.spacing(16)}>
       <Box>
-        <HeaderButton>Home</HeaderButton>
-        <HeaderButton>Blog</HeaderButton>
-        <HeaderButton>Contacts</HeaderButton>
+        <HeaderButton href="/">Home</HeaderButton>
+        <HeaderButton href="/" isDisabled>
+          Blog
+        </HeaderButton>
+        <HeaderButton href="/contacts" isDisabled>
+          Contacts
+        </HeaderButton>
       </Box>
     </Box>
   )
