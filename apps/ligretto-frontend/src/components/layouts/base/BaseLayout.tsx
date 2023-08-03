2d689ff132cc54fe503c298800cab0310f95a617
Roman Prokopchuk <prokopsasp@mail.ru>
2023-08-03 20:59:02 +0700

    [#374] changed BaseLayout to 90vh

--- apps/ligretto-frontend/src/components/layouts/base/BaseLayout.tsx
+++ apps/ligretto-frontend/src/components/layouts/base/BaseLayout.tsx
@@ -3,5 +3,5 @@ import type { BoxProps } from '@memebattle/ui'
 import { Box } from '@memebattle/ui'
 
 export const BaseLayout = (props: BoxProps) => (
-  <Box component="main" display="flex" flex={1} flexDirection="column" width="100%" height="100vh" {...props} />
+  <Box component="main" display="flex" flex={1} flexDirection="column" width="100%" height="90vh" {...props} />
 )
