2573ae1aebb22adf0117fcdd63c883ec24cd0f57
Artem Zverev <themezv@gmail.com>
2023-08-24 11:13:32 +0200

    [#402]: Fix blog dev command (#407)

--- apps/blog/src/components/Mdx/Mdx.tsx
+++ apps/blog/src/components/Mdx/Mdx.tsx
@@ -1,3 +1,4 @@
+import type { ImageProps } from 'next/image'
 import Image from 'next/image'
 import { useMDXComponent } from 'next-contentlayer/hooks'
 import InstagramPost from '../InstagramPost'
@@ -6,12 +7,14 @@ interface MdxProps {
   code: string
 }
 
+const components = { Image: (props: ImageProps) => <Image {...props} />, InstagramPost }
+
 export function Mdx({ code }: MdxProps) {
   const Component = useMDXComponent(code)
 
   return (
     <article className="prose lg:prose-xl">
-      <Component components={{ Image, InstagramPost }} />
+      <Component components={components} />
     </article>
   )
 }
