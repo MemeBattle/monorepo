9d842593d940261eadd05266c8aac33ec1dd9394
Roman Prokopchuk <prokopsasp@mail.ru>
2023-08-28 19:49:35 +0700

    [#374] GameLayout with fixed height 100vh/dvh

--- apps/ligretto-frontend/src/components/layouts/game/GameLayout.tsx
+++ apps/ligretto-frontend/src/components/layouts/game/GameLayout.tsx
@@ -1,7 +1,4 @@
 import { styled } from '@mui/material/styles'
 import { BaseLayout } from '../base/BaseLayout'
 
-export const GameLayout = styled(BaseLayout)(({ height }) => ({
-  '@supports (height: 1dvh)': { height: `${height}dvh` },
-  '@supports not (height: 1dvh)': { height: `${height}vh` },
-}))
+export const GameLayout = styled(BaseLayout)(() => ({ height: '100vh', '@supports (height: 1dvh)': { height: '100dvh' } }))
--- apps/ligretto-frontend/src/pages/game/GamePage.tsx
+++ apps/ligretto-frontend/src/pages/game/GamePage.tsx
@@ -1,13 +1,9 @@
 import React from 'react'
-
-import { MainLayout } from 'components/layouts/main/MainLayout'
 import { GamePageContainer } from 'containers/GamePageContainer'
 import { GameLayout } from 'components/layouts/game/GameLayout'
 
 export const GamePage: React.FC = () => (
-  <GameLayout height="100">
-    <MainLayout>
-      <GamePageContainer />
-    </MainLayout>
+  <GameLayout>
+    <GamePageContainer />
   </GameLayout>
 )
