53ada8a0a66524c37368b30219f550f77b9caa45
Guryanov-Maksim <guryanov-mv@yandex.ru>
2023-09-13 17:00:43 +0300

    [#326]: add a markdown to html convertor

---
+++ apps/blog/src/app/feed/[locale]/_utils/converter.ts
@@ -0,0 +1,17 @@
+import { unified } from 'unified'
+import remarkParse from 'remark-parse'
+import remarkRehype from 'remark-rehype'
+import rehypeSanitize from 'rehype-sanitize'
+import rehypeStringify from 'rehype-stringify'
+
+// TODO: see https://github.com/unifiedjs/unified/issues/227 before update the dependencies;
+export const convertMarkdownToHtml = async (content: string): Promise<string> => {
+  const file = await unified()
+    .use(remarkParse) // Convert into markdown AST
+    .use(remarkRehype) // Transform to HTML AST
+    .use(rehypeSanitize) // Sanitize HTML input
+    .use(rehypeStringify) // Convert AST into serialized HTML
+    .process(content)
+
+  return String(file)
+}
