6f35f323515f033c164f96d7d1f2bdc3e3c48aab
Guryanov-Maksim <guryanov-mv@yandex.ru>
2023-09-15 15:16:39 +0300

    [#326]: Remove duplicate code

--- apps/blog/src/i18n/index.ts
+++ apps/blog/src/i18n/index.ts
@@ -5,33 +5,36 @@ import { initReactI18next } from 'react-i18next/initReactI18next'
 import type { Language } from './i18n.settings'
 import { getOptions, defaultNS } from './i18n.settings'
 
-const initI18next = async (lng: string, ns?: string) => {
+enum AppSide {
+  Client,
+  Server,
+}
+
+const i18nextFactory = async (appSide: AppSide, lng: string, ns?: string) => {
   const i18nInstance = createInstance()
   await i18nInstance
-    .use(initReactI18next)
     .use(resourcesToBackend((language: string, namespace: string) => import(`./locales/${language}/${namespace}.json`)))
     .init(getOptions(lng, ns))
+
+  if (appSide === AppSide.Client) {
+    await i18nInstance.use(initReactI18next)
+  }
+
   return i18nInstance
 }
 
 export async function useTranslation(lng: Language, ns: keyof CustomTypeOptions['resources'] = defaultNS) {
-  const i18nextInstance = await initI18next(lng, ns)
+  const i18nextInstance = await i18nextFactory(AppSide.Client, lng, ns)
+
   return {
     t: i18nextInstance.getFixedT(lng, ns),
     i18n: i18nextInstance,
   }
 }
 
-const initI18nextForFeed = async (lng: string, ns?: string) => {
-  const i18nInstance = createInstance()
-  await i18nInstance
-    .use(resourcesToBackend((language: string, namespace: string) => import(`./locales/${language}/${namespace}.json`)))
-    .init(getOptions(lng, ns))
-  return i18nInstance
-}
-
 export async function getTranslation(lng: string, ns: keyof CustomTypeOptions['resources'] = defaultNS) {
-  const i18nextInstance = await initI18nextForFeed(lng, ns)
+  const i18nextInstance = await i18nextFactory(AppSide.Server, lng, ns)
+
   return {
     t: i18nextInstance.getFixedT(lng, ns),
     i18n: i18nextInstance,
