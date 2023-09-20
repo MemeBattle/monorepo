c0d0608ef82b3155d9d640fd2cb45d6c6fdd963f
Guryanov-Maksim <guryanov-mv@yandex.ru>
2023-09-20 16:28:59 +0300

    [#326]: Update feed test name

--- apps/blog/src/app/feed/[locale]/__tests__/route.test.ts
+++ apps/blog/src/app/feed/[locale]/__tests__/route.test.ts
@@ -7,12 +7,13 @@ import { GET } from '../route'
 
 const rawFeedXML = readFileSync(__dirname + '/feed.fixture.xml', 'utf8')
 const expectedFeedXML = rawFeedXML.trim()
+describe('Feed GET hundler', () => {
+  it("should return atom xml with blog's posts when a user requests it", async () => {
+    const request = new Request(`${process.env.VITE_APP_HOST_URL}/feed/en`)
+    const params: { locale: string } = { locale: 'en' }
+    const response = await GET(request, { params })
 
-it('should return the expected response', async () => {
-  const request = new Request(`${process.env.VITE_APP_HOST_URL}/feed/en`)
-  const params: { locale: string } = { locale: 'en' }
-  const response = await GET(request, { params })
-
-  expect(response.status).toEqual(200)
-  expect(await response.text()).toEqual(expectedFeedXML)
+    expect(response.status).toEqual(200)
+    expect(await response.text()).toEqual(expectedFeedXML)
+  })
 })
