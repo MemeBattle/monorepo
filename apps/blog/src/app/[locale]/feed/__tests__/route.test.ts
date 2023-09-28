dc7ab374ccf3243d247dcc73453650afeb6b0d44
Guryanov-Maksim <guryanov-mv@yandex.ru>
2023-09-28 12:45:42 +0300

    [#326]: Updates tests

--- apps/blog/src/app/[locale]/feed/__tests__/route.test.ts
+++ apps/blog/src/app/[locale]/feed/__tests__/route.test.ts
@@ -1,16 +1,16 @@
 import { readFileSync } from 'fs'
-import { testAllBlogPosts } from './allBlogPosts.fixture'
 
-vi.mock('contentlayer/generated', () => ({ allBlogPosts: testAllBlogPosts }))
+vi.mock('contentlayer/generated', () => ({ allBlogPosts: [JSON.parse(readFileSync(__dirname + '/testPost.fixture.json', 'utf8'))] }))
 
 import { GET } from '../route'
+import type { Language } from '@/i18n/i18n.settings'
 
 const rawFeedXML = readFileSync(__dirname + '/feed.fixture.xml', 'utf8')
 const expectedFeedXML = rawFeedXML.trim()
 describe('Feed GET hundler', () => {
   it("should return atom xml with blog's posts when a user requests it", async () => {
-    const request = new Request(`${process.env.VITE_APP_HOST_URL}/feed/en`)
-    const params: { locale: string } = { locale: 'en' }
+    const request = new Request(`${process.env.VITE_APP_HOST_URL}/en/feed`)
+    const params: { locale: Language } = { locale: 'en' }
     const response = await GET(request, { params })
 
     expect(response.status).toEqual(200)
