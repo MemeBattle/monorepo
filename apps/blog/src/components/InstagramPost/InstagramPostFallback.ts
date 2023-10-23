8e84405356fc7897d02b7b32a4030595795ea6d3
Guryanov-Maksim <guryanov-mv@yandex.ru>
2023-10-23 16:08:27 +0300

    [#438]: Replace string with component

--- apps/blog/src/components/InstagramPost/InstagramPostFallback.ts
+++
@@ -1,7 +0,0 @@
-export function InstagramPostFallback({ publicationId, description }: { publicationId: string; description: string }) {
-  return `<p>
-    <a href="https://www.instagram.com/p/${publicationId}" target="_blank">
-      ${description}. View this post on Instagram.
-    </a>
-  </p>`
-}
