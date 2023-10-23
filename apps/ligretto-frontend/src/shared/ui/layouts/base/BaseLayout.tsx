ffaf2e307b4e801c29cbfbd6353988f5ffeb1f83
Shilov Stas <sofabb9@gmail.com>
2023-10-24 00:26:47 +0700

    [#248] Add background texture

--- apps/ligretto-frontend/src/shared/ui/layouts/base/BaseLayout.tsx
+++ apps/ligretto-frontend/src/shared/ui/layouts/base/BaseLayout.tsx
@@ -1,7 +1,23 @@
 import React from 'react'
 import type { BoxProps } from '@memebattle/ui'
-import { Box } from '@memebattle/ui'
+import { useMediaQuery, useTheme, Box } from '@memebattle/ui'
 
-export const BaseLayout = (props: BoxProps) => (
-  <Box component="main" display="flex" flex={1} flexDirection="column" width="100%" height="100vh" {...props} />
-)
+import textureLarge from './assets/background-large-texture.png'
+import textureSmall from './assets/background-small-texture.png'
+
+export const BaseLayout = (props: BoxProps) => {
+  const theme = useTheme()
+  const isMobile = useMediaQuery(theme.breakpoints.down('sm'))
+  return (
+    <Box
+      component="main"
+      display="flex"
+      flex={1}
+      flexDirection="column"
+      width="100%"
+      height="100vh"
+      style={{ backgroundImage: `url(${!isMobile ? textureLarge : textureSmall}` }}
+      {...props}
+    />
+  )
+}
