1312f9cdc2bb1fb26a678122eab2fc652b9d3c85
Stonek79 <59825083+Stonek79@users.noreply.github.com>
2023-10-26 12:40:34 +0300

    [#455] styled images in Details block (#456)

--- apps/blog/tailwind.config.js
+++ apps/blog/tailwind.config.js
@@ -3,7 +3,7 @@ const defaultTheme = require('tailwindcss/defaultTheme')
 
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ['./src/**/*.{js,ts,jsx,tsx}', './contentlayer.config.js'],
+  content: ['./src/**/*.{js,ts,jsx,tsx}', './content/**/*.mdx', './contentlayer.config.js'],
   theme: {
     extend: {
       colors: {
