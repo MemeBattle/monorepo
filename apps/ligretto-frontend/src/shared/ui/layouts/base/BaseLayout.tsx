b1ce5672b6879c05d49ac7067ccf35926647f64f
Shilov Stas <sofabb9@gmail.com>
2023-10-30 22:57:39 +0700

    Заменяет фигуры на текстуру шума

--- apps/ligretto-frontend/src/shared/ui/layouts/base/BaseLayout.tsx
+++ apps/ligretto-frontend/src/shared/ui/layouts/base/BaseLayout.tsx
@@ -1,23 +1,18 @@
 import React from 'react'
 import type { BoxProps } from '@memebattle/ui'
-import { useMediaQuery, useTheme, Box } from '@memebattle/ui'
+import { Box } from '@memebattle/ui'
 
-import textureLarge from './assets/background-large-texture.png'
-import textureSmall from './assets/background-small-texture.png'
+import noisyTexture from './assets/noisy-texture.png'
 
-export const BaseLayout = (props: BoxProps) => {
-  const theme = useTheme()
-  const isMobile = useMediaQuery(theme.breakpoints.down('sm'))
-  return (
-    <Box
-      component="main"
-      display="flex"
-      flex={1}
-      flexDirection="column"
-      width="100%"
-      height="100vh"
-      style={{ backgroundImage: `url(${!isMobile ? textureLarge : textureSmall}` }}
-      {...props}
-    />
-  )
-}
+export const BaseLayout = (props: BoxProps) => (
+  <Box
+    component="main"
+    display="flex"
+    flex={1}
+    flexDirection="column"
+    width="100%"
+    height="100vh"
+    style={{ backgroundImage: `url(${noisyTexture}` }}
+    {...props}
+  />
+)
