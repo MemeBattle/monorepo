c96dde77c77faedb78154e21b0ddfbad43fbd385
Artem Zverev <themezv@gmail.com>
2024-03-23 00:11:02 +0100

    [#484]: fix ligretto-gameplay-backend

--- apps/ligretto-gameplay-backend/jest.config.ts
+++ apps/ligretto-gameplay-backend/jest.config.ts
@@ -2,13 +2,12 @@ import type { Config } from 'jest'
 
 const jestConfig: Config = {
   testEnvironment: 'node',
-  setupFiles: ['<rootDir>/test/setup.ts'],
   testRegex: '.*\\.spec\\.ts$',
   transform: {
     '^.+\\.ts$': [
       'ts-jest',
       {
-        tsconfig: 'tsconfig.spec.json',
+        tsconfig: 'tsconfig.json',
       },
     ],
   },
