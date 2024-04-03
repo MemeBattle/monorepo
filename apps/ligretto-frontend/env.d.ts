3c086da9c26602301b3638ab435ebc7b2b240ead
Artem Zverev <themezv@gmail.com>
2024-04-03 16:16:54 +0200

    [#217]: ligretto frontend sentry (#488)
    * [#217]: ligretto frontend sentry
    
    * [#217]: ligretto frontend sentry
    
    * [#217]: ligretto frontend sentry
    
    * [#217]: ligretto frontend sentry

--- apps/ligretto-frontend/env.d.ts
+++ apps/ligretto-frontend/env.d.ts
@@ -7,6 +7,9 @@ declare global {
     interface ProcessEnv {
       LIGRETTO_CORE_URL: string
       LIGRETTO_GAMEPLAY_URL: string
+      LIGRETTO_APP_ENV: string
+      LIGRETTO_APP_VERSION: string
+      LIGRETTO_FRONTEND_SENTRY_DSN: string
       CAS_STATIC_URL: string
       CAS_URL: string
       AMPLITUDE_TOKEN?: string
--- apps/ligretto-frontend/src/index.tsx
+++ apps/ligretto-frontend/src/index.tsx
@@ -1,3 +1,4 @@
+import '#app/sentry/initSentry'
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import { Provider } from 'react-redux'
