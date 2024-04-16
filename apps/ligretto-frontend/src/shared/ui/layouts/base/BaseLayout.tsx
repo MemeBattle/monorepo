1beaa6a07297eb251d207a97af9fdd82f1893a07
gjmazai <sofabb9@gmail.com>
2024-04-16 23:32:36 +0700

    Изменяет путь до текстуры

--- apps/ligretto-frontend/src/shared/ui/layouts/base/BaseLayout.tsx
+++ apps/ligretto-frontend/src/shared/ui/layouts/base/BaseLayout.tsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import type { BoxProps } from '@memebattle/ui'
 import { Box } from '@memebattle/ui'
-import image from '../../../../assets/images/noisy-texture.png'
+import image from './assets/noisy-texture.png'
 
 export const BaseLayout = (props: BoxProps) => (
   <Box component="main" display="flex" flex={1} flexDirection="column" width="100%" height="100vh" background={`url(${image})`} {...props} />
