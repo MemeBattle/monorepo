846dba8b49ed4f3c1370e2e94576c059c0924581
Shilov Stanislav <90744162+gjmazai@users.noreply.github.com>
2024-04-19 00:28:01 +0700

    [248] Ligretto Add backgound texture (#494)
    * [248] Ligretto Add backgound texture

--- apps/ligretto-frontend/src/shared/ui/layouts/base/BaseLayout.tsx
+++ apps/ligretto-frontend/src/shared/ui/layouts/base/BaseLayout.tsx
@@ -1,7 +1,8 @@
 import React from 'react'
 import type { BoxProps } from '@memebattle/ui'
 import { Box } from '@memebattle/ui'
+import image from './assets/noisy-texture.png'
 
 export const BaseLayout = (props: BoxProps) => (
-  <Box component="main" display="flex" flex={1} flexDirection="column" width="100%" height="100vh" {...props} />
+  <Box component="main" display="flex" flex={1} flexDirection="column" width="100%" height="100vh" background={`url(${image})`} {...props} />
 )
