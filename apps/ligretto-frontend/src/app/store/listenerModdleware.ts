ba965412194e1aa967611d70f9f8bccecd37a30c
bondiano <batbondik0@gmail.com>
2024-05-01 18:01:50 +0500

    WIP

--- apps/ligretto-frontend/src/app/store/listenerModdleware.ts
+++ apps/ligretto-frontend/src/app/store/listenerModdleware.ts
@@ -1,6 +1,7 @@
 import { createListenerMiddleware } from '@reduxjs/toolkit'
 
 import { addListeners as roomsAddListener } from '#ducks/rooms'
+import { addListeners as onboardingAddListener } from '#features/onboarding'
 import type { All } from '#types/store'
 
 export const listenerMiddleware = createListenerMiddleware()
@@ -8,3 +9,4 @@ export const listenerMiddleware = createListenerMiddleware()
 const startAppListening = listenerMiddleware.startListening.withTypes<All>()
 
 roomsAddListener(startAppListening)
+onboardingAddListener(startAppListening)
