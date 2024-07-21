d08a63d2da1fa3bab3c70c56509d208e09c09ce5
Artem Zverev <themezv@gmail.com>
2024-07-21 21:21:37 +0200

    layers

--- apps/ligretto-frontend/src/features/onboarding/index.ts
+++ apps/ligretto-frontend/src/features/onboarding/index.ts
@@ -1,1 +1,12 @@
 export { addListeners } from './model/listeners'
+export { OnboardingStep } from './model/fsm'
+export {
+  putFirstCardAction,
+  putLigrettoCardAction,
+  putSecondCardAction,
+  putStackCardAction,
+  putThirdCardAction,
+  onboardingGameSelector,
+  nextStepOnboardingAction,
+  onboardingStepSelector,
+} from './model/slice'
