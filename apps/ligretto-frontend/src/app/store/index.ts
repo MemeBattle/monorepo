9ba88478c77686695d08a2af526f5ad2e226e111
Artem Zverev <themezv@gmail.com>
2025-10-12 19:07:08 +0200

    refactor(ligretto-frontend): remove sagas

--- apps/ligretto-frontend/src/app/store/index.ts
+++ apps/ligretto-frontend/src/app/store/index.ts
@@ -1,24 +1,16 @@
 import { configureStore } from '@reduxjs/toolkit'
-import createSagaMiddleware from 'redux-saga'
 
 import { IS_DEV_MODE } from '#shared/constants/config'
 
-import rootSaga from './rootSaga'
 import { routerMiddleware, createReduxHistory } from './reduxHistoryContext'
 import { rootReducer } from './rootReducer'
 import { listenerMiddleware } from './listenerMiddleware'
-
-const sagaMiddleware = createSagaMiddleware()
 
 export const store = configureStore({
   reducer: rootReducer,
   devTools: IS_DEV_MODE,
   middleware: getDefaultMiddleware =>
-    getDefaultMiddleware({ serializableCheck: false, thunk: false })
-      .concat([routerMiddleware, sagaMiddleware])
-      .prepend(listenerMiddleware.middleware),
+    getDefaultMiddleware({ serializableCheck: false, thunk: false }).concat([routerMiddleware]).prepend(listenerMiddleware.middleware),
 })
 
 export const history = createReduxHistory(store)
-
-sagaMiddleware.run(rootSaga)
--- apps/ligretto-frontend/src/entities/socket/index.ts
+++ apps/ligretto-frontend/src/entities/socket/index.ts
@@ -1,2 +1,3 @@
 export { socketSaga } from './model/socket-saga'
 export { socketConnectedAction } from './model/actions'
+export { addListeners } from './model/listeners'
