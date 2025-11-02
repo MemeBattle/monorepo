94a6762c176a4ca0a166721df76c510aabdaf90b
Artem Zverev <themezv@gmail.com>
2025-11-02 23:44:43 +0100

    wip

--- apps/ligretto-frontend/src/pages/onboarding/Layer.tsx
+++ apps/ligretto-frontend/src/pages/onboarding/Layer.tsx
@@ -6,13 +6,43 @@ import { useSelector } from 'react-redux'
 
 type LayerId = 'playerCards' | 'playgroundCards' | 'opponent'
 
+/**
+ * TopLayer is a record of layer ids and the onboarding steps that should be above them
+ */
 const TopLayer: Record<LayerId, Set<OnboardingStep>> = {
-  playerCards: new Set([OnboardingStep.Cards, OnboardingStep.Stack, OnboardingStep.Row, OnboardingStep.Ligretto]),
-  playgroundCards: new Set([OnboardingStep.Playground]),
+  playerCards: new Set([
+    OnboardingStep.Cards,
+    OnboardingStep.Stack,
+    OnboardingStep.Row,
+    OnboardingStep.Ligretto,
+    OnboardingStep.FirstCard,
+    OnboardingStep.LigrettoCard,
+    OnboardingStep.StackAvailableCard,
+    OnboardingStep.StackCard,
+    OnboardingStep.StackUnavailableCard,
+    OnboardingStep.StackAvailableCard,
+    OnboardingStep.RowAvailableCard,
+    OnboardingStep.LigrettoAvailableCard,
+    OnboardingStep.GameStarted,
+    OnboardingStep.GameStartedCycledInfo,
+  ]),
+  playgroundCards: new Set([
+    OnboardingStep.Playground,
+    OnboardingStep.FirstCard,
+    OnboardingStep.LigrettoCard,
+    OnboardingStep.StackAvailableCard,
+    OnboardingStep.StackCard,
+    OnboardingStep.StackUnavailableCard,
+    OnboardingStep.StackAvailableCard,
+    OnboardingStep.RowAvailableCard,
+    OnboardingStep.LigrettoAvailableCard,
+    OnboardingStep.GameStarted,
+    OnboardingStep.GameStartedCycledInfo,
+  ]),
   opponent: new Set([OnboardingStep.Opponents]),
 }
 
-function isCardAbove(layerId: LayerId, currentStep: OnboardingStep): boolean {
+function isLayerAbove(layerId: LayerId, currentStep: OnboardingStep): boolean {
   return TopLayer[layerId].has(currentStep)
 }
 
@@ -20,7 +50,7 @@ export function Layer({ id, children }: PropsWithChildren<{ id: LayerId }>) {
   const currentStep = useSelector(onboardingStepSelector)
 
   return (
-    <Box position="relative" zIndex={isCardAbove(id, currentStep) ? 1 : undefined}>
+    <Box position="relative" zIndex={isLayerAbove(id, currentStep) ? 1 : undefined}>
       {children}
     </Box>
   )
