1685ac53ff8d1176b26975f25083794b5fa7091d
Artem Zverev <themezv@gmail.com>
2026-04-12 22:08:34 +0200

    wip

--- apps/blog/next.config.ts
+++ apps/blog/next.config.ts
@@ -1,3 +1,5 @@
+import type { NextConfig } from 'next'
+import path from 'path'
 import createMDX from '@next/mdx'
 import remarkFrontmatter from 'remark-frontmatter'
 import rehypeSlug from 'rehype-slug'
@@ -5,32 +7,32 @@ import rehypeAutolinkHeadings from 'rehype-autolink-headings'
 
 const withMDX = createMDX({
   options: {
-    remarkPlugins: [remarkFrontmatter],
-    rehypePlugins: [
-      rehypeSlug,
-      [
-        rehypeAutolinkHeadings,
-        {
-          behavior: 'wrap',
-          properties: {
-            className: ['no-underline hover:underline font-bold text-inherit'],
-          },
-        },
-      ],
-    ],
+    // remarkPlugins: [remarkFrontmatter],
+    // rehypePlugins: [
+    //   'rehypeSlug',
+    //   [
+    //     rehypeAutolinkHeadings,
+    //     {
+    //       behavior: 'wrap',
+    //       properties: {
+    //         className: ['no-underline hover:underline font-bold text-inherit'],
+    //       },
+    //     },
+    //   ],
+    // ],
   },
 })
 
-/** @type {import('next').NextConfig} */
 const nextConfig = {
+  experimental: { mdxRs: true },
   reactStrictMode: true,
   output: 'standalone',
   pageExtensions: ['js', 'jsx', 'ts', 'tsx', 'md', 'mdx'],
-  eslint: {
-    ignoreDuringBuilds: true,
-  },
   typescript: {
     ignoreBuildErrors: true,
+  },
+  turbopack: {
+    root: path.resolve(__dirname, '../..'),
   },
   async rewrites() {
     return [
@@ -40,6 +42,6 @@ const nextConfig = {
       },
     ]
   },
-}
+} satisfies NextConfig
 
 export default withMDX(nextConfig)
